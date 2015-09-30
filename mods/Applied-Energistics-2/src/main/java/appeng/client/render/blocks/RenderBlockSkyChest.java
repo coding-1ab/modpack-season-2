AppliedEnergistics/Applied-Energistics-2

Remove redundant type casts

src/main/java/appeng/client/render/blocks/RenderBlockSkyChest.java
@@ -81,14 +81,12 @@ public class RenderBlockSkyChest extends BaseBlockRender<BlockSkyChest, TileSkyC
 	}
 
 	@Override
-	public void renderTile( BlockSkyChest block, TileSkyChest tile, Tessellator tess, double x, double y, double z, float partialTick, RenderBlocks renderer )
+	public void renderTile( BlockSkyChest block, TileSkyChest skyChest, Tessellator tess, double x, double y, double z, float partialTick, RenderBlocks renderer )
 	{
-		if( !( tile instanceof TileSkyChest ) )
+		if(  skyChest == null )
 		{
 			return;
 		}
-
-		final TileSkyChest skyChest = (TileSkyChest) tile;
 
 		if( !skyChest.hasWorldObj() )
 		{
@@ -98,7 +96,7 @@ public class RenderBlockSkyChest extends BaseBlockRender<BlockSkyChest, TileSkyC
 		GL11.glEnable( GL12.GL_RESCALE_NORMAL );
 		GL11.glColor4f( 1.0F, 1.0F, 1.0F, 1.0F );
 
-		final int metaData = tile.getWorldObj().getBlockMetadata( tile.xCoord, tile.yCoord, tile.zCoord );
+		final int metaData = skyChest.getWorldObj().getBlockMetadata( skyChest.xCoord, skyChest.yCoord, skyChest.zCoord );
 		final ResourceLocation loc = METADATA_TO_TEXTURE[metaData];
 
 		Minecraft.getMinecraft().getTextureManager().bindTexture( loc );

src/main/java/appeng/client/render/blocks/RenderBlockSkyCompass.java
@@ -179,14 +179,12 @@ public class RenderBlockSkyCompass extends BaseBlockRender<BlockSkyCompass, Tile
 	}
 
 	@Override
-	public void renderTile( BlockSkyCompass block, TileSkyCompass tile, Tessellator tess, double x, double y, double z, float partialTick, RenderBlocks renderer )
+	public void renderTile( BlockSkyCompass block, TileSkyCompass skyCompass, Tessellator tess, double x, double y, double z, float partialTick, RenderBlocks renderer )
 	{
-		if( !( tile instanceof TileSkyCompass ) )
+		if( skyCompass == null )
 		{
 			return;
 		}
-
-		TileSkyCompass skyCompass = (TileSkyCompass) tile;
 
 		if( !skyCompass.hasWorldObj() )
 		{
@@ -197,7 +195,7 @@ public class RenderBlockSkyCompass extends BaseBlockRender<BlockSkyCompass, Tile
 		GL11.glColor4f( 1.0F, 1.0F, 1.0F, 1.0F );
 		GL11.glCullFace( GL11.GL_FRONT );
 
-		ResourceLocation loc = new ResourceLocation( "appliedenergistics2", "textures/models/compass.png" );
+		final ResourceLocation loc = new ResourceLocation( "appliedenergistics2", "textures/models/compass.png" );
 
 		Minecraft.getMinecraft().getTextureManager().bindTexture( loc );
 
@@ -211,7 +209,7 @@ public class RenderBlockSkyCompass extends BaseBlockRender<BlockSkyCompass, Tile
 		CompassResult cr = null;
 		if( skyCompass.getForward() == ForgeDirection.UP || skyCompass.getForward() == ForgeDirection.DOWN )
 		{
-			cr = CompassManager.INSTANCE.getCompassDirection( 0, tile.xCoord, tile.yCoord, tile.zCoord );
+			cr = CompassManager.INSTANCE.getCompassDirection( 0, skyCompass.xCoord, skyCompass.yCoord, skyCompass.zCoord );
 		}
 		else
 		{

src/main/java/appeng/client/render/blocks/RendererCableBus.java
@@ -65,12 +65,12 @@ public class RendererCableBus extends BaseBlockRender<BlockCableBus, TileCableBu
 	}
 
 	@Override
-	public void renderTile( BlockCableBus block, TileCableBus t, Tessellator tess, double x, double y, double z, float f, RenderBlocks renderer )
+	public void renderTile( BlockCableBus block, TileCableBus cableBus, Tessellator tess, double x, double y, double z, float f, RenderBlocks renderer )
 	{
-		if( t instanceof TileCableBus )
+		if( cableBus != null )
 		{
 			BusRenderer.INSTANCE.renderer.overrideBlockTexture = null;
-			( (TileCableBus) t ).cb.renderDynamic( x, y, z );
+			cableBus.cb.renderDynamic( x, y, z );
 		}
 	}
 }

coding-1ab/modpack-season-2

mods/Applied-Energistics-2/src/main/java/appeng/client/render/blocks/RenderBlockSkyChest.java
@@ -84,14 +84,12 @@ public class RenderBlockSkyChest extends BaseBlockRender<BlockSkyChest, TileSkyC
 	}
 
 	@Override
-	public void renderTile( BlockSkyChest block, TileSkyChest tile, WorldRenderer tess, double x, double y, double z, float partialTick, ModelGenerator renderer )
+	public void renderTile( BlockSkyChest block, TileSkyChest skyChest, WorldRenderer tess, double x, double y, double z, float partialTick, ModelGenerator renderer )
 	{
-		if( !( tile instanceof TileSkyChest ) )
+		if(  skyChest == null )
 		{
 			return;
 		}
-
-		final TileSkyChest skyChest = tile;
 
 		if( !skyChest.hasWorldObj() )
 		{
@@ -101,7 +99,7 @@ public class RenderBlockSkyChest extends BaseBlockRender<BlockSkyChest, TileSkyC
 		GL11.glEnable( GL12.GL_RESCALE_NORMAL );
 		GL11.glColor4f( 1.0F, 1.0F, 1.0F, 1.0F );
 
-		final IBlockState metaData = tile.getWorld().getBlockState( tile.getPos() );
+		final IBlockState metaData = skyChest.getWorld().getBlockState( skyChest.getPos() );
 		final ResourceLocation loc = METADATA_TO_TEXTURE[ ((BlockSkyChest)metaData.getBlock()).type == SkyChestType.BLOCK ? 1 : 0 ];
 
 		Minecraft.getMinecraft().getTextureManager().bindTexture( loc );

mods/Applied-Energistics-2/src/main/java/appeng/client/render/blocks/RenderBlockSkyCompass.java
@@ -182,14 +182,12 @@ public class RenderBlockSkyCompass extends BaseBlockRender<BlockSkyCompass, Tile
 	}
 
 	@Override
-	public void renderTile( BlockSkyCompass block, TileSkyCompass tile, WorldRenderer tess, double x, double y, double z, float partialTick, ModelGenerator renderer )
+	public void renderTile( BlockSkyCompass block, TileSkyCompass skyCompass, WorldRenderer tess, double x, double y, double z, float partialTick, ModelGenerator renderer )
 	{
-		if( !( tile instanceof TileSkyCompass ) )
+		if( skyCompass == null )
 		{
 			return;
 		}
-
-		TileSkyCompass skyCompass = tile;
 
 		if( !skyCompass.hasWorldObj() )
 		{
@@ -200,7 +198,7 @@ public class RenderBlockSkyCompass extends BaseBlockRender<BlockSkyCompass, Tile
 		GL11.glColor4f( 1.0F, 1.0F, 1.0F, 1.0F );
 		GL11.glCullFace( GL11.GL_FRONT );
 
-		ResourceLocation loc = new ResourceLocation( "appliedenergistics2", "textures/models/compass.png" );
+		final ResourceLocation loc = new ResourceLocation( "appliedenergistics2", "textures/models/compass.png" );
 
 		Minecraft.getMinecraft().getTextureManager().bindTexture( loc );
 
@@ -214,7 +212,8 @@ public class RenderBlockSkyCompass extends BaseBlockRender<BlockSkyCompass, Tile
 		CompassResult cr = null;
 		if( skyCompass.getForward() == EnumFacing.UP || skyCompass.getForward() == EnumFacing.DOWN )
 		{
-			cr = CompassManager.INSTANCE.getCompassDirection( 0, tile.getPos().getX(), tile.getPos().getY(),tile.getPos().getZ());
+			final BlockPos compassPos = skyCompass.getPos();
+			cr = CompassManager.INSTANCE.getCompassDirection( 0, compassPos.getX(), compassPos.getY(), compassPos.getZ());
 		}
 		else
 		{

mods/Applied-Energistics-2/src/main/java/appeng/client/render/blocks/RendererCableBus.java
@@ -136,12 +136,12 @@ public class RendererCableBus extends BaseBlockRender<BlockCableBus, TileCableBu
 	}
 
 	@Override
-	public void renderTile( BlockCableBus block, TileCableBus t, WorldRenderer tess, double x, double y, double z, float f, ModelGenerator renderer )
+	public void renderTile( BlockCableBus block, TileCableBus cableBus, WorldRenderer tess, double x, double y, double z, float f, ModelGenerator renderer )
 	{
-		if( t instanceof TileCableBus )
+		if( cableBus != null )
 		{
 			BusRenderer.INSTANCE.renderer.overrideBlockTexture = null;
-			t.cb.renderDynamic( x, y, z );
+			cableBus.cb.renderDynamic( x, y, z );
 		}
 	}
 }