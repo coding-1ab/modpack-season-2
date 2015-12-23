AppliedEnergistics/Applied-Energistics-2

Fixes #1983: WorldData cleared too early when still needed.

src/main/java/appeng/core/AppEng.java
@@ -21,6 +21,7 @@ package appeng.core;
 
 import java.io.File;
 import java.util.concurrent.TimeUnit;
+
 import javax.annotation.Nonnull;
 
 import com.google.common.base.Stopwatch;
@@ -37,6 +38,7 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerAboutToStartEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
+import cpw.mods.fml.common.event.FMLServerStoppedEvent;
 import cpw.mods.fml.common.event.FMLServerStoppingEvent;
 import cpw.mods.fml.common.network.NetworkRegistry;
 
@@ -233,6 +235,12 @@ public final class AppEng
 	private void serverStopping( final FMLServerStoppingEvent event )
 	{
 		WorldData.instance().onServerStopping();
+	}
+
+	@EventHandler
+	private void serverStopped( final FMLServerStoppedEvent event )
+	{
+		WorldData.instance().onServerStoppped();
 		TickHandler.INSTANCE.shutdown();
 	}
 

coding-1ab/modpack-season-2

mods/Applied-Energistics-2/src/main/java/appeng/core/AppEng.java
@@ -37,6 +37,7 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerAboutToStartEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import net.minecraftforge.fml.common.event.FMLServerStoppedEvent;
 import net.minecraftforge.fml.common.event.FMLServerStoppingEvent;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
 
@@ -237,6 +238,12 @@ public final class AppEng
 	private void serverStopping( final FMLServerStoppingEvent event )
 	{
 		WorldData.instance().onServerStopping();
+	}
+
+	@EventHandler
+	private void serverStopped( final FMLServerStoppedEvent event )
+	{
+		WorldData.instance().onServerStoppped();
 		TickHandler.INSTANCE.shutdown();
 	}
 