AppliedEnergistics/Applied-Energistics-2

Reduces visibility of internal fields/methods
Reduces the visibility of all fields to private and create setters/getters
when necessary. Exceptions are fields with GuiSync as these need to be
public.

Reduces the visibility of internal methods to private/protected/default when possible.

src/main/java/appeng/parts/misc/PartCableAnchor.java
@@ -53,10 +53,10 @@ import appeng.api.parts.PartItemStack;
 public class PartCableAnchor implements IPart
 {
 
-	protected ISimplifiedBundle renderCache = null;
-	ItemStack is = null;
-	IPartHost host = null;
-	ForgeDirection mySide = ForgeDirection.UP;
+	private ISimplifiedBundle renderCache = null;
+	private ItemStack is = null;
+	private IPartHost host = null;
+	private ForgeDirection mySide = ForgeDirection.UP;
 
 	public PartCableAnchor( final ItemStack is )
 	{

coding-1ab/modpack-season-2

mods/Applied-Energistics-2/src/main/java/appeng/parts/misc/PartCableAnchor.java
@@ -52,9 +52,9 @@ import appeng.client.texture.IAESprite;
 public class PartCableAnchor implements IPart
 {
 
-	ItemStack is = null;
-	IPartHost host = null;
-	AEPartLocation mySide = AEPartLocation.UP;
+	private ItemStack is = null;
+	private IPartHost host = null;
+	private AEPartLocation mySide = AEPartLocation.UP;
 
 	public PartCableAnchor( final ItemStack is )
 	{
@@ -94,7 +94,7 @@ public class PartCableAnchor implements IPart
 	@SideOnly( Side.CLIENT )
 	public void renderStatic( final BlockPos pos, final IPartRenderHelper rh, final ModelGenerator renderer )
 	{
-		final IAESprite myIcon =  renderer.getIcon( this.is );
+		final IAESprite myIcon = renderer.getIcon( this.is );
 		rh.setTexture( myIcon );
 		if( this.host != null && this.host.getFacadeContainer().getFacade( this.mySide ) != null )
 		{
@@ -116,7 +116,7 @@ public class PartCableAnchor implements IPart
 	}
 
 	@Override
-	public TextureAtlasSprite getBreakingTexture( final ModelGenerator renderer)
+	public TextureAtlasSprite getBreakingTexture( final ModelGenerator renderer )
 	{
 		return null;
 	}