DragonsPlusMinecraft/CreateCentralKitchen

Farmer's Respite module WIP

src/main/java/plus/dragons/createcentralkitchen/modules/farmersrespite/FarmersRespiteModule.java
@@ -1,0 +1,98 @@
+package plus.dragons.createcentralkitchen.modules.farmersrespite;
+
+import com.farmersrespite.core.registry.FRBlockEntityTypes;
+import com.mojang.logging.LogUtils;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+import plus.dragons.createcentralkitchen.common.modules.ModModule;
+import plus.dragons.createcentralkitchen.modules.farmersdelight.FarmersDelightModule;
+import plus.dragons.createcentralkitchen.modules.farmersdelight.content.contraptions.blazeStove.BlazeStoveBlockEntity;
+import plus.dragons.createcentralkitchen.modules.farmersdelight.entry.FdItems;
+import plus.dragons.createcentralkitchen.modules.farmersdelight.entry.FdMenuTypes;
+import plus.dragons.createcentralkitchen.modules.farmersrespite.content.logistics.block.mechanicalArm.FrArmInteractionPointTypes;
+import plus.dragons.createcentralkitchen.modules.farmersrespite.entry.FrCapabilities;
+import plus.dragons.createcentralkitchen.modules.farmersrespite.entry.FrItems;
+import plus.dragons.createcentralkitchen.modules.farmersrespite.entry.FrMenuTypes;
+
+@ModModule(id = "farmersrespite", dependencies = {"farmersdelight", "farmersrespite"}, priority = 1)
+public class FarmersRespiteModule {
+    private static final Logger LOGGER = LogUtils.getLogger();
+    public static final String ID = "farmersrespite";
+    
+    public FarmersRespiteModule() {
+        IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
+        IEventBus forgeBus = MinecraftForge.EVENT_BUS;
+        
+        registerEntries();
+        registerModEvents(modBus);
+        registerForgeEvents(forgeBus);
+        
+        DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> FarmersDelightModule.Client::new);
+    }
+    
+    private void registerEntries() {
+        FrItems.register();
+        FrMenuTypes.register();
+        //FrFluids.register();
+        FrArmInteractionPointTypes.register();
+    }
+    
+    private void registerModEvents(IEventBus modBus) {
+        modBus.addListener(FrCapabilities::register);
+        modBus.addListener(this::setup);
+    }
+    
+    private void registerForgeEvents(IEventBus forgeBus) {
+        forgeBus.addListener(FrItems::fillCreateItemGroup);
+    }
+    
+    public void setup(final FMLCommonSetupEvent event) {
+        event.enqueueWork(() -> {
+            BlazeStoveBlockEntity.registerBoostingCooker(FRBlockEntityTypes.KETTLE.get());
+        });
+    }
+    
+    public static ResourceLocation genRL(String path) {
+        return new ResourceLocation(ID, path);
+    }
+    
+    public static class Client {
+        
+        public Client() {
+            IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
+            IEventBus forgeBus = MinecraftForge.EVENT_BUS;
+            
+            registerEntries();
+            registerModEvents(modBus);
+            registerForgeEvents(forgeBus);
+        }
+        
+        private void registerEntries() {
+        
+        }
+        
+        private void registerModEvents(IEventBus modBus) {
+            modBus.addListener(this::setup);
+        }
+        
+        private void registerForgeEvents(IEventBus forgeBus) {
+        
+        }
+        
+        public void setup(final FMLClientSetupEvent event) {
+            event.enqueueWork(() -> {
+                //FdPonderIndex.register();
+                //FdPonderIndex.registerTags();
+            });
+        }
+        
+    }
+    
+}

src/main/java/plus/dragons/createcentralkitchen/modules/farmersrespite/content/logistics/item/guide/BrewingGuide.java
@@ -1,0 +1,75 @@
+package plus.dragons.createcentralkitchen.modules.farmersrespite.content.logistics.item.guide;
+
+import com.farmersrespite.core.registry.FRRecipeTypes;
+import net.minecraft.MethodsReturnNonnullByDefault;
+import net.minecraft.core.Direction;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.util.LazyOptional;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import plus.dragons.createcentralkitchen.modules.farmersdelight.content.contraptions.blazeStove.BlazeStoveGuide;
+import plus.dragons.createcentralkitchen.modules.farmersrespite.entry.FrCapabilities;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+public class BrewingGuide extends BlazeStoveGuide {
+    private final LazyOptional<BrewingGuide> capability = LazyOptional.of(() -> this);
+    private boolean needWater;
+    
+    public BrewingGuide(ItemStack owner) {
+        super(owner, 2);
+    }
+    
+    public static BrewingGuide of(ItemStack stack) {
+        return stack.getCapability(FrCapabilities.BREWING_GUIDE).orElseThrow(() ->
+            new UnsupportedOperationException("Requested Item " + stack.getItem() + " is not a Brewing Guide"));
+    }
+    
+    public boolean needWater() {
+        return needWater;
+    }
+    
+    @Override
+    public CompoundTag serializeNBT() {
+        CompoundTag tag = super.serializeNBT();
+        tag.putBoolean("NeedWater", needWater);
+        return tag;
+    }
+    
+    @Override
+    public void deserializeNBT(CompoundTag tag) {
+        super.deserializeNBT(tag);
+        needWater = tag.getBoolean("NeedWater");
+    }
+    
+    @NotNull
+    @Override
+    public <T> LazyOptional<T> getCapability(@NotNull Capability<T> cap, @Nullable Direction side) {
+        if (cap == FrCapabilities.BREWING_GUIDE) {
+            return capability.cast();
+        }
+        return LazyOptional.empty();
+    }
+    
+    public void updateRecipe(Level level) {
+        level.getRecipeManager()
+            .getRecipeFor(FRRecipeTypes.BREWING.get(), recipeWrapper, level)
+            .ifPresentOrElse(
+                recipe -> {
+                    inventory.setStackInSlot(ingredientSize, recipe.getResultItem());
+                    container = recipe.getOutputContainer();
+                    needWater = recipe.getNeedWater();
+                },
+                () -> {
+                    inventory.setStackInSlot(ingredientSize, ItemStack.EMPTY);
+                    container = ItemStack.EMPTY;
+                    needWater = false;
+                });
+    }
+    
+}

src/main/java/plus/dragons/createcentralkitchen/modules/farmersrespite/entry/FrItems.java
@@ -1,5 +1,6 @@
-package plus.dragons.createcentralkitchen.modules.farmersdelight.entry;
+package plus.dragons.createcentralkitchen.modules.farmersrespite.entry;
 
+import com.farmersrespite.core.tag.FRTags;
 import com.simibubi.create.AllBlocks;
 import com.simibubi.create.Create;
 import com.simibubi.create.content.AllSections;
@@ -9,31 +10,31 @@ import net.minecraft.resources.ResourceLocation;
 import plus.dragons.createcentralkitchen.CentralKitchen;
 import plus.dragons.createcentralkitchen.common.item.FillCreateItemGroupEvent;
 import plus.dragons.createcentralkitchen.data.recipe.ConditionedRecipes;
-import plus.dragons.createcentralkitchen.modules.farmersdelight.FarmersDelightModule;
-import plus.dragons.createcentralkitchen.modules.farmersdelight.content.logistics.item.guide.CookingGuideItem;
+import plus.dragons.createcentralkitchen.modules.farmersdelight.entry.FdTags;
+import plus.dragons.createcentralkitchen.modules.farmersrespite.FarmersRespiteModule;
+import plus.dragons.createcentralkitchen.modules.farmersrespite.content.logistics.item.guide.BrewingGuideItem;
 import vectorwing.farmersdelight.common.registry.ModItems;
-import vectorwing.farmersdelight.common.tag.ForgeTags;
 
-public class FdItems {
+public class FrItems {
     
     private static final CreateRegistrate REGISTRATE = CentralKitchen.REGISTRATE
         .startSection(AllSections.KINETICS)
         .creativeModeTab(() -> Create.BASE_CREATIVE_TAB);
 
-    public static final ItemEntry<CookingGuideItem> COOKING_GUIDE = REGISTRATE.item("cooking_guide", CookingGuideItem::new)
+    public static final ItemEntry<BrewingGuideItem> BREWING_GUIDE = REGISTRATE.item("brewing_guide", BrewingGuideItem::new)
         .properties(prop -> prop.stacksTo(1))
         .recipe((ctx, prov) -> ConditionedRecipes.shapeless(ctx.getEntry())
-            .requires(FdTags.item(new ResourceLocation("forge", "plates/obsidian")))
+            .requires(FdTags.item(new ResourceLocation("forge", "plates/copper")))
             .requires(ModItems.CANVAS.get())
-            .requires(ForgeTags.VEGETABLES)
-            .whenModLoaded(FarmersDelightModule.ID)
+            .requires(FRTags.TEA_LEAVES)
+            .whenModLoaded(FarmersRespiteModule.ID)
             .save(prov)
         )
         .register();
 
     public static void fillCreateItemGroup(FillCreateItemGroupEvent event) {
         if (event.getItemGroup() == Create.BASE_CREATIVE_TAB) {
-            event.addInsertion(AllBlocks.BLAZE_BURNER.get(), COOKING_GUIDE.asStack());
+            event.addInsertion(AllBlocks.BLAZE_BURNER.get(), BREWING_GUIDE.asStack());
         }
     }
 

src/main/java/plus/dragons/createcentralkitchen/modules/farmersrespite/integration/jei/transfer/BrewingGuideTransferHandler.java
@@ -1,5 +1,6 @@
-package plus.dragons.createcentralkitchen.modules.farmersdelight.integration.jei.transfer;
+package plus.dragons.createcentralkitchen.modules.farmersrespite.integration.jei.transfer;
 
+import com.farmersrespite.common.crafting.KettleRecipe;
 import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
 import mezz.jei.api.recipe.transfer.IRecipeTransferError;
 import mezz.jei.api.recipe.transfer.IRecipeTransferHandler;
@@ -9,32 +10,31 @@ import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.Nullable;
 import plus.dragons.createcentralkitchen.common.network.CentralKitchenNetwork;
 import plus.dragons.createcentralkitchen.modules.farmersdelight.content.contraptions.blazeStove.BlazeStoveGuideSyncPacket;
-import plus.dragons.createcentralkitchen.modules.farmersdelight.content.logistics.item.guide.CookingGuideMenu;
-import vectorwing.farmersdelight.common.crafting.CookingPotRecipe;
+import plus.dragons.createcentralkitchen.modules.farmersrespite.content.logistics.item.guide.BrewingGuideMenu;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
-public class CookingGuideTransferHandler implements IRecipeTransferHandler<CookingGuideMenu, CookingPotRecipe> {
+public class BrewingGuideTransferHandler implements IRecipeTransferHandler<BrewingGuideMenu, KettleRecipe> {
     
     @Override
-    public Class<CookingGuideMenu> getContainerClass() {
-        return CookingGuideMenu.class;
+    public Class<BrewingGuideMenu> getContainerClass() {
+        return BrewingGuideMenu.class;
     }
     
     @Override
-    public Class<CookingPotRecipe> getRecipeClass() {
-        return CookingPotRecipe.class;
+    public Class<KettleRecipe> getRecipeClass() {
+        return KettleRecipe.class;
     }
     
     @Override
     @Nullable
-    public IRecipeTransferError transferRecipe(CookingGuideMenu container, CookingPotRecipe recipe, IRecipeSlotsView recipeSlots, Player player, boolean maxTransfer, boolean doTransfer) {
+    public IRecipeTransferError transferRecipe(BrewingGuideMenu container, KettleRecipe recipe, IRecipeSlotsView recipeSlots, Player player, boolean maxTransfer, boolean doTransfer) {
         if (!doTransfer)
             return null;
         var inputs = recipe.getIngredients();
-        for (int i = 0; i < 6; ++i) {
+        for (int i = 0; i < 2; ++i) {
             ItemStack input;
             if (i < inputs.size()) {
                 var items = inputs.get(i).getItems();

coding-1ab/modpack-season-2

mods/CreateCentralKitchen/src/main/java/plus/dragons/createcentralkitchen/modules/farmersrespite/FarmersRespiteModule.java
@@ -1,0 +1,96 @@
+package plus.dragons.createcentralkitchen.modules.farmersrespite;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+import plus.dragons.createcentralkitchen.common.modules.ModModule;
+import plus.dragons.createcentralkitchen.modules.farmersdelight.FarmersDelightModule;
+import plus.dragons.createcentralkitchen.modules.farmersdelight.content.contraptions.blazeStove.BlazeStoveBlockEntity;
+import plus.dragons.createcentralkitchen.modules.farmersrespite.content.logistics.block.mechanicalArm.FrArmInteractionPointTypes;
+import plus.dragons.createcentralkitchen.modules.farmersrespite.entry.FrCapabilities;
+import plus.dragons.createcentralkitchen.modules.farmersrespite.entry.FrItems;
+import plus.dragons.createcentralkitchen.modules.farmersrespite.entry.FrMenuTypes;
+import umpaz.farmersrespite.common.registry.FRBlockEntityTypes;
+
+@ModModule(id = "farmersrespite", dependencies = {"farmersdelight", "farmersrespite"}, priority = 1)
+public class FarmersRespiteModule {
+    private static final Logger LOGGER = LogUtils.getLogger();
+    public static final String ID = "farmersrespite";
+    
+    public FarmersRespiteModule() {
+        IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
+        IEventBus forgeBus = MinecraftForge.EVENT_BUS;
+        
+        registerEntries();
+        registerModEvents(modBus);
+        registerForgeEvents(forgeBus);
+        
+        DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> FarmersDelightModule.Client::new);
+    }
+    
+    private void registerEntries() {
+        FrItems.register();
+        FrMenuTypes.register();
+        //FrFluids.register();
+        FrArmInteractionPointTypes.register();
+    }
+    
+    private void registerModEvents(IEventBus modBus) {
+        modBus.addListener(FrCapabilities::register);
+        modBus.addListener(this::setup);
+    }
+    
+    private void registerForgeEvents(IEventBus forgeBus) {
+        forgeBus.addListener(FrItems::fillCreateItemGroup);
+    }
+    
+    public void setup(final FMLCommonSetupEvent event) {
+        event.enqueueWork(() -> {
+            BlazeStoveBlockEntity.registerBoostingCooker(FRBlockEntityTypes.KETTLE.get());
+        });
+    }
+    
+    public static ResourceLocation genRL(String path) {
+        return new ResourceLocation(ID, path);
+    }
+    
+    public static class Client {
+        
+        public Client() {
+            IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
+            IEventBus forgeBus = MinecraftForge.EVENT_BUS;
+            
+            registerEntries();
+            registerModEvents(modBus);
+            registerForgeEvents(forgeBus);
+        }
+        
+        private void registerEntries() {
+        
+        }
+        
+        private void registerModEvents(IEventBus modBus) {
+            modBus.addListener(this::setup);
+        }
+        
+        private void registerForgeEvents(IEventBus forgeBus) {
+        
+        }
+        
+        public void setup(final FMLClientSetupEvent event) {
+            event.enqueueWork(() -> {
+                //FdPonderIndex.register();
+                //FdPonderIndex.registerTags();
+            });
+        }
+        
+    }
+    
+}

mods/CreateCentralKitchen/src/main/java/plus/dragons/createcentralkitchen/modules/farmersrespite/content/logistics/item/guide/BrewingGuide.java
@@ -1,0 +1,75 @@
+package plus.dragons.createcentralkitchen.modules.farmersrespite.content.logistics.item.guide;
+
+import net.minecraft.MethodsReturnNonnullByDefault;
+import net.minecraft.core.Direction;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.util.LazyOptional;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import plus.dragons.createcentralkitchen.modules.farmersdelight.content.contraptions.blazeStove.BlazeStoveGuide;
+import plus.dragons.createcentralkitchen.modules.farmersrespite.entry.FrCapabilities;
+import umpaz.farmersrespite.common.registry.FRRecipeTypes;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+public class BrewingGuide extends BlazeStoveGuide {
+    private final LazyOptional<BrewingGuide> capability = LazyOptional.of(() -> this);
+    private boolean needWater;
+    
+    public BrewingGuide(ItemStack owner) {
+        super(owner, 2);
+    }
+    
+    public static BrewingGuide of(ItemStack stack) {
+        return stack.getCapability(FrCapabilities.BREWING_GUIDE).orElseThrow(() ->
+            new UnsupportedOperationException("Requested Item " + stack.getItem() + " is not a Brewing Guide"));
+    }
+    
+    public boolean needWater() {
+        return needWater;
+    }
+    
+    @Override
+    public CompoundTag serializeNBT() {
+        CompoundTag tag = super.serializeNBT();
+        tag.putBoolean("NeedWater", needWater);
+        return tag;
+    }
+    
+    @Override
+    public void deserializeNBT(CompoundTag tag) {
+        super.deserializeNBT(tag);
+        needWater = tag.getBoolean("NeedWater");
+    }
+    
+    @NotNull
+    @Override
+    public <T> LazyOptional<T> getCapability(@NotNull Capability<T> cap, @Nullable Direction side) {
+        if (cap == FrCapabilities.BREWING_GUIDE) {
+            return capability.cast();
+        }
+        return LazyOptional.empty();
+    }
+    
+    public void updateRecipe(Level level) {
+        level.getRecipeManager()
+            .getRecipeFor(FRRecipeTypes.BREWING.get(), recipeWrapper, level)
+            .ifPresentOrElse(
+                recipe -> {
+                    inventory.setStackInSlot(ingredientSize, recipe.getResultItem());
+                    container = recipe.getOutputContainer();
+                    needWater = recipe.getNeedWater();
+                },
+                () -> {
+                    inventory.setStackInSlot(ingredientSize, ItemStack.EMPTY);
+                    container = ItemStack.EMPTY;
+                    needWater = false;
+                });
+    }
+    
+}

mods/CreateCentralKitchen/src/main/java/plus/dragons/createcentralkitchen/modules/farmersrespite/entry/FrItems.java
@@ -1,4 +1,4 @@
-package plus.dragons.createcentralkitchen.modules.farmersdelight.entry;
+package plus.dragons.createcentralkitchen.modules.farmersrespite.entry;
 
 import com.simibubi.create.AllBlocks;
 import com.simibubi.create.Create;
@@ -8,32 +8,33 @@ import com.tterrag.registrate.util.entry.ItemEntry;
 import net.minecraft.resources.ResourceLocation;
 import plus.dragons.createcentralkitchen.CentralKitchen;
 import plus.dragons.createcentralkitchen.data.recipe.ConditionedRecipes;
-import plus.dragons.createcentralkitchen.modules.farmersdelight.FarmersDelightModule;
+import plus.dragons.createcentralkitchen.modules.farmersdelight.entry.FdTags;
+import plus.dragons.createcentralkitchen.modules.farmersrespite.FarmersRespiteModule;
+import plus.dragons.createcentralkitchen.modules.farmersrespite.content.logistics.item.guide.BrewingGuideItem;
 import plus.dragons.createdragonlib.init.FillCreateItemGroupEvent;
-import plus.dragons.createcentralkitchen.modules.farmersdelight.content.logistics.item.guide.CookingGuideItem;
+import umpaz.farmersrespite.common.tag.FRTags;
 import vectorwing.farmersdelight.common.registry.ModItems;
-import vectorwing.farmersdelight.common.tag.ForgeTags;
 
-public class FdItems {
+public class FrItems {
     
     private static final CreateRegistrate REGISTRATE = CentralKitchen.REGISTRATE
         .startSection(AllSections.KINETICS)
         .creativeModeTab(() -> Create.BASE_CREATIVE_TAB);
 
-    public static final ItemEntry<CookingGuideItem> COOKING_GUIDE = REGISTRATE.item("cooking_guide", CookingGuideItem::new)
+    public static final ItemEntry<BrewingGuideItem> BREWING_GUIDE = REGISTRATE.item("brewing_guide", BrewingGuideItem::new)
         .properties(prop -> prop.stacksTo(1))
         .recipe((ctx, prov) -> ConditionedRecipes.shapeless(ctx.getEntry())
-            .requires(FdTags.item(new ResourceLocation("forge", "plates/obsidian")))
+            .requires(FdTags.item(new ResourceLocation("forge", "plates/copper")))
             .requires(ModItems.CANVAS.get())
-            .requires(ForgeTags.VEGETABLES)
-            .whenModLoaded(FarmersDelightModule.ID)
+            .requires(FRTags.TEA_LEAVES)
+            .whenModLoaded(FarmersRespiteModule.ID)
             .save(prov)
         )
         .register();
 
     public static void fillCreateItemGroup(FillCreateItemGroupEvent event) {
         if (event.getItemGroup() == Create.BASE_CREATIVE_TAB) {
-            event.addInsertion(AllBlocks.BLAZE_BURNER.get(), COOKING_GUIDE.asStack());
+            event.addInsertion(AllBlocks.BLAZE_BURNER.get(), BREWING_GUIDE.asStack());
         }
     }
 

mods/CreateCentralKitchen/src/main/java/plus/dragons/createcentralkitchen/modules/farmersrespite/integration/jei/transfer/BrewingGuideTransferHandler.java
@@ -1,4 +1,4 @@
-package plus.dragons.createcentralkitchen.modules.farmersdelight.integration.jei.transfer;
+package plus.dragons.createcentralkitchen.modules.farmersrespite.integration.jei.transfer;
 
 import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
 import mezz.jei.api.recipe.RecipeType;
@@ -11,40 +11,40 @@ import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.Nullable;
 import plus.dragons.createcentralkitchen.CentralKitchen;
 import plus.dragons.createcentralkitchen.common.network.CentralKitchenNetwork;
-import plus.dragons.createcentralkitchen.modules.farmersdelight.entry.FdMenuTypes;
 import plus.dragons.createcentralkitchen.modules.farmersdelight.content.contraptions.blazeStove.BlazeStoveGuideSyncPacket;
-import plus.dragons.createcentralkitchen.modules.farmersdelight.content.logistics.item.guide.CookingGuideMenu;
-import vectorwing.farmersdelight.common.crafting.CookingPotRecipe;
+import plus.dragons.createcentralkitchen.modules.farmersrespite.content.logistics.item.guide.BrewingGuideMenu;
+import plus.dragons.createcentralkitchen.modules.farmersrespite.entry.FrMenuTypes;
+import umpaz.farmersrespite.common.crafting.KettleRecipe;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.Optional;
 
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
-public class CookingGuideTransferHandler implements IRecipeTransferHandler<CookingGuideMenu, CookingPotRecipe> {
+public class BrewingGuideTransferHandler implements IRecipeTransferHandler<BrewingGuideMenu, KettleRecipe> {
     
     @Override
-    public Class<CookingGuideMenu> getContainerClass() {
-        return CookingGuideMenu.class;
+    public Class<BrewingGuideMenu> getContainerClass() {
+        return BrewingGuideMenu.class;
     }
 
     @Override
-    public Optional<MenuType<CookingGuideMenu>> getMenuType() {
-        return Optional.of(FdMenuTypes.COOKING_GUIDE.get());
+    public Optional<MenuType<BrewingGuideMenu>> getMenuType() {
+        return Optional.of(FrMenuTypes.BREWING_GUIDE.get());
     }
 
     @Override
-    public RecipeType<CookingPotRecipe> getRecipeType() {
-        return RecipeType.create(CentralKitchen.ID, "cooking", CookingPotRecipe.class);
+    public RecipeType<KettleRecipe> getRecipeType() {
+        return RecipeType.create(CentralKitchen.ID, "brewing", KettleRecipe.class);
     }
     
     @Override
     @Nullable
-    public IRecipeTransferError transferRecipe(CookingGuideMenu container, CookingPotRecipe recipe, IRecipeSlotsView recipeSlots, Player player, boolean maxTransfer, boolean doTransfer) {
+    public IRecipeTransferError transferRecipe(BrewingGuideMenu container, KettleRecipe recipe, IRecipeSlotsView recipeSlots, Player player, boolean maxTransfer, boolean doTransfer) {
         if (!doTransfer)
             return null;
         var inputs = recipe.getIngredients();
-        for (int i = 0; i < 6; ++i) {
+        for (int i = 0; i < 2; ++i) {
             ItemStack input;
             if (i < inputs.size()) {
                 var items = inputs.get(i).getItems();