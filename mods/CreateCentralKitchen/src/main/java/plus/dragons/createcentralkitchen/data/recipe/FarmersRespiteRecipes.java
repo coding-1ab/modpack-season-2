DragonsPlusMinecraft/CreateCentralKitchen

Farmer's Respite recipes

src/main/java/plus/dragons/createcentralkitchen/data/recipe/FarmersRespiteRecipes.java
@@ -1,0 +1,51 @@
+package plus.dragons.createcentralkitchen.data.recipe;
+
+import com.farmersrespite.core.registry.FRItems;
+import com.farmersrespite.core.tag.FRTags;
+import com.simibubi.create.AllFluids;
+import com.simibubi.create.AllItems;
+import com.simibubi.create.content.contraptions.processing.HeatCondition;
+import com.simibubi.create.foundation.data.recipe.CreateRecipeProvider.GeneratedRecipe;
+import net.minecraft.MethodsReturnNonnullByDefault;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.material.Fluids;
+import net.minecraftforge.common.Tags;
+import vectorwing.farmersdelight.common.registry.ModItems;
+
+@MethodsReturnNonnullByDefault
+public class FarmersRespiteRecipes extends RecipeGen {
+    private final GeneratedRecipe
+    COFFEE_BEAN_MILLING = modded(milling("coffee_beans")
+        .output(FRItems.COFFEE_BEANS.get())
+        .output(Items.RED_DYE)
+        .require(FRItems.COFFEE_BERRIES.get())),
+    YELLOW_TEA_LEAVES_HAUNTING = modded(haunting("yellow_tea_leaves")
+        .output(FRItems.YELLOW_TEA_LEAVES.get())
+        .require(FRItems.GREEN_TEA_LEAVES.get())),
+    BLACK_TEA_LEAVES_HAUNTING = modded(haunting("black_tea_leaves")
+        .output(FRItems.BLACK_TEA_LEAVES.get())
+        .require(FRItems.YELLOW_TEA_LEAVES.get())),
+    BUILDERS_TEA_FROM_TEA_LEAVES_MIXING = modded(mixing("builders_tea_from_tea_leaves")
+        .output(AllFluids.TEA.get(), 500)
+        .require(Fluids.WATER, 250)
+        .require(Tags.Fluids.MILK, 250)
+        .require(FRTags.TEA_LEAVES)
+        .requiresHeat(HeatCondition.HEATED)),
+    BUILDERS_TEA_KETTLE = modded(kettle("builders_tea")
+        .output(AllItems.BUILDERS_TEA.get())
+        .require(FRTags.TEA_LEAVES)
+        .require(ModItems.MILK_BOTTLE.get())
+        .container(Items.GLASS)
+        .needWater()),
+    FINISH = null;
+    
+    public FarmersRespiteRecipes(DataGenerator datagen) {
+        super("farmersrespite", datagen);
+    }
+    
+    @Override
+    public String getName() {
+        return "Farmer's Respite Recipes";
+    }
+}

src/main/java/plus/dragons/createcentralkitchen/data/recipe/builder/ConditionedKettleRecipeBuilder.java
@@ -1,0 +1,158 @@
+package plus.dragons.createcentralkitchen.data.recipe.builder;
+
+import com.farmersrespite.data.builder.KettleRecipeBuilder;
+import com.google.common.collect.Lists;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import net.minecraft.MethodsReturnNonnullByDefault;
+import net.minecraft.data.recipes.FinishedRecipe;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.level.ItemLike;
+import net.minecraftforge.common.crafting.CraftingHelper;
+import net.minecraftforge.common.crafting.conditions.ICondition;
+
+import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+public class ConditionedKettleRecipeBuilder implements ConditionedRecipeBuilder<ConditionedKettleRecipeBuilder> {
+    private final ResourceLocation id;
+    private Item result;
+    private int count = 1;
+    private int duration = 2400;
+    private float experience = 0.35F;
+    private Item container = null;
+    private boolean needWater = false;
+    private final List<Ingredient> ingredients = Lists.newArrayList();
+    private final List<ICondition> conditions = new ArrayList<>();
+    
+    public ConditionedKettleRecipeBuilder(ResourceLocation id) {
+        this.id = id;
+    }
+    
+    public ConditionedKettleRecipeBuilder require(TagKey<Item> tagIn) {
+        return this.require(Ingredient.of(tagIn));
+    }
+    
+    public ConditionedKettleRecipeBuilder require(ItemLike itemIn) {
+        return this.require(itemIn, 1);
+    }
+    
+    public ConditionedKettleRecipeBuilder require(ItemLike itemIn, int count) {
+        for(int i = 0; i < count; ++i) {
+            this.require(Ingredient.of(itemIn));
+        }
+        
+        return this;
+    }
+    
+    public ConditionedKettleRecipeBuilder require(Ingredient ingredientIn) {
+        return this.require(ingredientIn, 1);
+    }
+    
+    public ConditionedKettleRecipeBuilder require(Ingredient ingredientIn, int count) {
+        for(int i = 0; i < count; ++i) {
+            this.ingredients.add(ingredientIn);
+        }
+        return this;
+    }
+    
+    public ConditionedKettleRecipeBuilder duration(int ticks) {
+        this.duration = ticks;
+        return this;
+    }
+    
+    public ConditionedKettleRecipeBuilder experience(float experience) {
+        this.experience = experience;
+        return this;
+    }
+    
+    public ConditionedKettleRecipeBuilder experience(double experience) {
+        this.experience = (float) experience;
+        return this;
+    }
+    
+    public ConditionedKettleRecipeBuilder container(@Nullable Item container) {
+        this.container = container;
+        return this;
+    }
+    
+    public ConditionedKettleRecipeBuilder needWater() {
+        this.needWater = true;
+        return this;
+    }
+    
+    public ConditionedKettleRecipeBuilder output(ItemLike item, int count) {
+        this.result = item.asItem();
+        this.count = count;
+        return this;
+    }
+    
+    public ConditionedKettleRecipeBuilder output(ItemLike item) {
+        return output(item, 1);
+    }
+    
+    @Override
+    public ConditionedKettleRecipeBuilder withCondition(ICondition condition) {
+        this.conditions.add(condition);
+        return this ;
+    }
+    
+    @Override
+    public void save(Consumer<FinishedRecipe> consumer) {
+        this.save(consumer, this.id);
+    }
+    
+    @Override
+    public void save(Consumer<FinishedRecipe> consumer, ResourceLocation id) {
+        id = new ResourceLocation(id.getNamespace(), "brewing/" + id.getPath());
+        this.ensureValid(id);
+        consumer.accept(new Result(id, this.result, this.count,
+            this.ingredients, this.duration, this.experience,
+            this.needWater, this.container, this.conditions));
+    }
+    
+    /**
+     * Makes sure that this recipe is valid
+     */
+    private void ensureValid(ResourceLocation id) {
+        if (this.result == null) {
+            throw new IllegalStateException("No result is defined for kettle brewing recipe " + id + "!");
+        }
+    }
+    
+    public static class Result extends KettleRecipeBuilder.Result {
+        private final List<ICondition> conditions;
+        
+        public Result(ResourceLocation id,
+                      Item result,
+                      int count,
+                      List<Ingredient> ingredient,
+                      int duration,
+                      float experience,
+                      boolean needWater,
+                      Item container,
+                      List<ICondition> conditions) {
+            super(id, result, count, ingredient, duration, experience, needWater, container);
+            this.conditions = conditions;
+        }
+        
+        public void serializeRecipeData(JsonObject json) {
+            super.serializeRecipeData(json);
+            if (!conditions.isEmpty()) {
+                JsonArray conds = new JsonArray();
+                conditions.forEach(c -> conds.add(CraftingHelper.serialize(c)));
+                json.add("conditions", conds);
+            }
+        }
+        
+    }
+    
+}

src/main/java/plus/dragons/createcentralkitchen/data/tag/CentralKitchenTags.java
@@ -1,7 +1,8 @@
-package plus.dragons.createcentralkitchen.modules.farmersdelight.entry;
+package plus.dragons.createcentralkitchen.data.tag;
 
+import com.farmersrespite.core.registry.FRBlocks;
 import com.simibubi.create.AllBlocks;
-import com.simibubi.create.Create;
+import com.simibubi.create.AllTags;
 import com.tterrag.registrate.providers.ProviderType;
 import com.tterrag.registrate.providers.RegistrateItemTagsProvider;
 import com.tterrag.registrate.providers.RegistrateTagsProvider;
@@ -12,17 +13,15 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.material.Fluid;
 import plus.dragons.createcentralkitchen.CentralKitchen;
+import vectorwing.farmersdelight.common.registry.ModBlocks;
 import vectorwing.farmersdelight.common.registry.ModItems;
 import vectorwing.farmersdelight.common.tag.ForgeTags;
 import vectorwing.farmersdelight.common.tag.ModTags;
 
 import static plus.dragons.createcentralkitchen.CentralKitchen.REGISTRATE;
 
-public class FdTags {
-    public static final TagKey<Item> UPRIGHT_ON_BELT = item(Create.asResource("upright_on_belt"));
+public class CentralKitchenTags {
     public static final TagKey<Item> UPRIGHT_ON_DEPLOYER = item(CentralKitchen.genRL("upright_on_deployer"));
-    public static final TagKey<Block> FAN_TRANSPARENT = block(Create.asResource("fan_transparent"));
-    public static final TagKey<Block> FAN_HEATERS = block(Create.asResource("fan_heaters"));
     
     public static TagKey<Item> item(ResourceLocation id) {
         return TagKey.create(Registry.ITEM_REGISTRY, id);
@@ -37,7 +36,7 @@ public class FdTags {
     }
     
     public static void genItemTags(RegistrateItemTagsProvider prov) {
-        prov.tag(UPRIGHT_ON_BELT)
+        prov.tag(AllTags.AllItemTags.UPRIGHT_ON_BELT.tag)
             .addOptional(ModItems.APPLE_CIDER.getId())
             .addOptional(ModItems.HOT_COCOA.getId())
             .addOptional(ModItems.MELON_JUICE.getId())
@@ -49,11 +48,14 @@ public class FdTags {
         prov.tag(ModTags.TRAY_HEAT_SOURCES)
             .add(AllBlocks.LIT_BLAZE_BURNER.get())
             .add(AllBlocks.BLAZE_BURNER.get());
+        prov.tag(AllTags.AllBlockTags.WRENCH_PICKUP.tag)
+            .addOptional(ModBlocks.COOKING_POT.getId())
+            .addOptional(FRBlocks.KETTLE.getId());
     }
     
     public static void register() {
-        REGISTRATE.addDataGenerator(ProviderType.ITEM_TAGS, FdTags::genItemTags);
-        REGISTRATE.addDataGenerator(ProviderType.BLOCK_TAGS, FdTags::genBlockTags);
+        REGISTRATE.addDataGenerator(ProviderType.ITEM_TAGS, CentralKitchenTags::genItemTags);
+        REGISTRATE.addDataGenerator(ProviderType.BLOCK_TAGS, CentralKitchenTags::genBlockTags);
     }
     
 }

coding-1ab/modpack-season-2

mods/CreateCentralKitchen/src/main/java/plus/dragons/createcentralkitchen/data/recipe/FarmersRespiteRecipes.java
@@ -1,0 +1,51 @@
+package plus.dragons.createcentralkitchen.data.recipe;
+
+import com.simibubi.create.AllFluids;
+import com.simibubi.create.AllItems;
+import com.simibubi.create.content.contraptions.processing.HeatCondition;
+import com.simibubi.create.foundation.data.recipe.CreateRecipeProvider.GeneratedRecipe;
+import net.minecraft.MethodsReturnNonnullByDefault;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.material.Fluids;
+import net.minecraftforge.common.Tags;
+import umpaz.farmersrespite.common.registry.FRItems;
+import umpaz.farmersrespite.common.tag.FRTags;
+import vectorwing.farmersdelight.common.registry.ModItems;
+
+@MethodsReturnNonnullByDefault
+public class FarmersRespiteRecipes extends RecipeGen {
+    private final GeneratedRecipe
+    COFFEE_BEAN_MILLING = modded(milling("coffee_beans")
+        .output(FRItems.COFFEE_BEANS.get())
+        .output(Items.RED_DYE)
+        .require(FRItems.COFFEE_BERRIES.get())),
+    YELLOW_TEA_LEAVES_HAUNTING = modded(haunting("yellow_tea_leaves")
+        .output(FRItems.YELLOW_TEA_LEAVES.get())
+        .require(FRItems.GREEN_TEA_LEAVES.get())),
+    BLACK_TEA_LEAVES_HAUNTING = modded(haunting("black_tea_leaves")
+        .output(FRItems.BLACK_TEA_LEAVES.get())
+        .require(FRItems.YELLOW_TEA_LEAVES.get())),
+    BUILDERS_TEA_FROM_TEA_LEAVES_MIXING = modded(mixing("builders_tea_from_tea_leaves")
+        .output(AllFluids.TEA.get(), 500)
+        .require(Fluids.WATER, 250)
+        .require(Tags.Fluids.MILK, 250)
+        .require(FRTags.TEA_LEAVES)
+        .requiresHeat(HeatCondition.HEATED)),
+    BUILDERS_TEA_KETTLE = modded(kettle("builders_tea")
+        .output(AllItems.BUILDERS_TEA.get())
+        .require(FRTags.TEA_LEAVES)
+        .require(ModItems.MILK_BOTTLE.get())
+        .container(Items.GLASS)
+        .needWater()),
+    FINISH = null;
+    
+    public FarmersRespiteRecipes(DataGenerator datagen) {
+        super("farmersrespite", datagen);
+    }
+    
+    @Override
+    public String getName() {
+        return "Farmer's Respite Recipes";
+    }
+}

mods/CreateCentralKitchen/src/main/java/plus/dragons/createcentralkitchen/data/recipe/builder/ConditionedKettleRecipeBuilder.java
@@ -1,0 +1,166 @@
+package plus.dragons.createcentralkitchen.data.recipe.builder;
+
+import com.google.common.collect.Lists;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import net.minecraft.MethodsReturnNonnullByDefault;
+import net.minecraft.data.recipes.FinishedRecipe;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.level.ItemLike;
+import net.minecraftforge.common.crafting.CraftingHelper;
+import net.minecraftforge.common.crafting.conditions.ICondition;
+import umpaz.farmersrespite.data.builder.KettleRecipeBuilder;
+
+import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+public class ConditionedKettleRecipeBuilder implements ConditionedRecipeBuilder<ConditionedKettleRecipeBuilder> {
+    private final ResourceLocation id;
+    private Item result;
+    private int count = 1;
+    private int duration = 2400;
+    private float experience = 0.35F;
+    private Item container = null;
+    private boolean needWater = false;
+    private final List<Ingredient> ingredients = Lists.newArrayList();
+    private final List<ICondition> conditions = new ArrayList<>();
+    @Nullable
+    private String group;
+    
+    public ConditionedKettleRecipeBuilder(ResourceLocation id) {
+        this.id = id;
+    }
+    
+    public ConditionedKettleRecipeBuilder require(TagKey<Item> tagIn) {
+        return this.require(Ingredient.of(tagIn));
+    }
+    
+    public ConditionedKettleRecipeBuilder require(ItemLike itemIn) {
+        return this.require(itemIn, 1);
+    }
+    
+    public ConditionedKettleRecipeBuilder require(ItemLike itemIn, int count) {
+        for(int i = 0; i < count; ++i) {
+            this.require(Ingredient.of(itemIn));
+        }
+        
+        return this;
+    }
+    
+    public ConditionedKettleRecipeBuilder require(Ingredient ingredientIn) {
+        return this.require(ingredientIn, 1);
+    }
+    
+    public ConditionedKettleRecipeBuilder require(Ingredient ingredientIn, int count) {
+        for(int i = 0; i < count; ++i) {
+            this.ingredients.add(ingredientIn);
+        }
+        return this;
+    }
+
+    public ConditionedKettleRecipeBuilder group(@Nullable String group) {
+        this.group = group;
+        return this;
+    }
+    
+    public ConditionedKettleRecipeBuilder duration(int ticks) {
+        this.duration = ticks;
+        return this;
+    }
+    
+    public ConditionedKettleRecipeBuilder experience(float experience) {
+        this.experience = experience;
+        return this;
+    }
+    
+    public ConditionedKettleRecipeBuilder experience(double experience) {
+        this.experience = (float) experience;
+        return this;
+    }
+    
+    public ConditionedKettleRecipeBuilder container(@Nullable Item container) {
+        this.container = container;
+        return this;
+    }
+    
+    public ConditionedKettleRecipeBuilder needWater() {
+        this.needWater = true;
+        return this;
+    }
+    
+    public ConditionedKettleRecipeBuilder output(ItemLike item, int count) {
+        this.result = item.asItem();
+        this.count = count;
+        return this;
+    }
+    
+    public ConditionedKettleRecipeBuilder output(ItemLike item) {
+        return output(item, 1);
+    }
+    
+    @Override
+    public ConditionedKettleRecipeBuilder withCondition(ICondition condition) {
+        this.conditions.add(condition);
+        return this ;
+    }
+    
+    @Override
+    public void save(Consumer<FinishedRecipe> consumer) {
+        this.save(consumer, this.id);
+    }
+    
+    @Override
+    public void save(Consumer<FinishedRecipe> consumer, ResourceLocation id) {
+        id = new ResourceLocation(id.getNamespace(), "brewing/" + id.getPath());
+        this.ensureValid(id);
+        consumer.accept(new Result(id, this.result, this.count, this.group == null ? "": this.group,
+            this.ingredients, this.duration, this.experience,
+            this.needWater, this.container, this.conditions));
+    }
+    
+    /**
+     * Makes sure that this recipe is valid
+     */
+    private void ensureValid(ResourceLocation id) {
+        if (this.result == null) {
+            throw new IllegalStateException("No result is defined for kettle brewing recipe " + id + "!");
+        }
+    }
+    
+    public static class Result extends KettleRecipeBuilder.Result {
+        private final List<ICondition> conditions;
+        
+        public Result(ResourceLocation id,
+                      Item result,
+                      int count,
+                      String group,
+                      List<Ingredient> ingredient,
+                      int duration,
+                      float experience,
+                      boolean needWater,
+                      Item container,
+                      List<ICondition> conditions) {
+            super(id, result, count, group, ingredient, duration,  needWater, experience, container, null);
+            this.conditions = conditions;
+        }
+        
+        public void serializeRecipeData(JsonObject json) {
+            super.serializeRecipeData(json);
+            if (!conditions.isEmpty()) {
+                JsonArray conds = new JsonArray();
+                conditions.forEach(c -> conds.add(CraftingHelper.serialize(c)));
+                json.add("conditions", conds);
+            }
+        }
+        
+    }
+    
+}

mods/CreateCentralKitchen/src/main/java/plus/dragons/createcentralkitchen/data/tag/CentralKitchenTags.java
@@ -1,7 +1,7 @@
-package plus.dragons.createcentralkitchen.modules.farmersdelight.entry;
+package plus.dragons.createcentralkitchen.data.tag;
 
 import com.simibubi.create.AllBlocks;
-import com.simibubi.create.Create;
+import com.simibubi.create.AllTags;
 import com.tterrag.registrate.providers.ProviderType;
 import com.tterrag.registrate.providers.RegistrateItemTagsProvider;
 import com.tterrag.registrate.providers.RegistrateTagsProvider;
@@ -12,17 +12,16 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.material.Fluid;
 import plus.dragons.createcentralkitchen.CentralKitchen;
+import umpaz.farmersrespite.common.registry.FRBlocks;
+import vectorwing.farmersdelight.common.registry.ModBlocks;
 import vectorwing.farmersdelight.common.registry.ModItems;
 import vectorwing.farmersdelight.common.tag.ForgeTags;
 import vectorwing.farmersdelight.common.tag.ModTags;
 
 import static plus.dragons.createcentralkitchen.CentralKitchen.REGISTRATE;
 
-public class FdTags {
-    public static final TagKey<Item> UPRIGHT_ON_BELT = item(Create.asResource("upright_on_belt"));
+public class CentralKitchenTags {
     public static final TagKey<Item> UPRIGHT_ON_DEPLOYER = item(CentralKitchen.genRL("upright_on_deployer"));
-    public static final TagKey<Block> FAN_TRANSPARENT = block(Create.asResource("fan_transparent"));
-    public static final TagKey<Block> FAN_HEATERS = block(Create.asResource("fan_heaters"));
     
     public static TagKey<Item> item(ResourceLocation id) {
         return TagKey.create(Registry.ITEM_REGISTRY, id);
@@ -37,7 +36,7 @@ public class FdTags {
     }
     
     public static void genItemTags(RegistrateItemTagsProvider prov) {
-        prov.tag(UPRIGHT_ON_BELT)
+        prov.tag(AllTags.AllItemTags.UPRIGHT_ON_BELT.tag)
             .addOptional(ModItems.APPLE_CIDER.getId())
             .addOptional(ModItems.HOT_COCOA.getId())
             .addOptional(ModItems.MELON_JUICE.getId())
@@ -49,11 +48,14 @@ public class FdTags {
         prov.tag(ModTags.TRAY_HEAT_SOURCES)
             .add(AllBlocks.LIT_BLAZE_BURNER.get())
             .add(AllBlocks.BLAZE_BURNER.get());
+        prov.tag(AllTags.AllBlockTags.WRENCH_PICKUP.tag)
+            .addOptional(ModBlocks.COOKING_POT.getId())
+            .addOptional(FRBlocks.KETTLE.getId());
     }
     
     public static void register() {
-        REGISTRATE.addDataGenerator(ProviderType.ITEM_TAGS, FdTags::genItemTags);
-        REGISTRATE.addDataGenerator(ProviderType.BLOCK_TAGS, FdTags::genBlockTags);
+        REGISTRATE.addDataGenerator(ProviderType.ITEM_TAGS, CentralKitchenTags::genItemTags);
+        REGISTRATE.addDataGenerator(ProviderType.BLOCK_TAGS, CentralKitchenTags::genBlockTags);
     }
     
 }