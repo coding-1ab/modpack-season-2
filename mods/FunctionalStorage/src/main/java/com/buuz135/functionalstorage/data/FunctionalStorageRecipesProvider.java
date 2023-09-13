Buuz135/FunctionalStorage

Added custom compacting recipes, changed recipes that used the tag to use the custom recipe, closes #150

src/main/java/com/buuz135/functionalstorage/data/FunctionalStorageRecipesProvider.java
@@ -1,0 +1,126 @@
+package com.buuz135.functionalstorage.data;
+
+import com.buuz135.functionalstorage.item.StorageUpgradeItem;
+import com.buuz135.functionalstorage.util.StorageTags;
+import com.hrznstudio.titanium.block.BasicBlock;
+import com.hrznstudio.titanium.recipe.generator.TitaniumRecipeProvider;
+import com.hrznstudio.titanium.recipe.generator.TitaniumShapedRecipeBuilder;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.recipes.FinishedRecipe;
+import net.minecraft.data.recipes.UpgradeRecipeBuilder;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.common.Tags;
+import net.minecraftforge.common.util.NonNullLazy;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+import static com.buuz135.functionalstorage.FunctionalStorage.*;
+
+public class FunctionalStorageRecipesProvider extends TitaniumRecipeProvider {
+
+    private NonNullLazy<List<Block>> blocksToProcess;
+
+    public FunctionalStorageRecipesProvider(DataGenerator generator, NonNullLazy<List<Block>> blocksToProcess) {
+        super(generator);
+        this.blocksToProcess = blocksToProcess;
+    }
+
+    @Override
+    public void register(Consumer<FinishedRecipe> consumer) {
+        blocksToProcess.get().stream().map(block -> (BasicBlock) block).forEach(basicBlock -> basicBlock.registerRecipe(consumer));
+        TitaniumShapedRecipeBuilder.shapedRecipe(STORAGE_UPGRADES.get(StorageUpgradeItem.StorageTier.IRON).get())
+                .pattern("III").pattern("IDI").pattern("III")
+                .define('I', Tags.Items.INGOTS_IRON)
+                .define('D', StorageTags.DRAWER)
+                .save(consumer);
+        TitaniumShapedRecipeBuilder.shapedRecipe(VOID_UPGRADE.get())
+                .pattern("III").pattern("IDI").pattern("III")
+                .define('I', Tags.Items.OBSIDIAN)
+                .define('D', StorageTags.DRAWER)
+                .save(consumer);
+        TitaniumShapedRecipeBuilder.shapedRecipe(CONFIGURATION_TOOL.get())
+                .pattern("PPG").pattern("PDG").pattern("PEP")
+                .define('P', Items.PAPER)
+                .define('G', Tags.Items.INGOTS_GOLD)
+                .define('D', StorageTags.DRAWER)
+                .define('E', Items.EMERALD)
+                .save(consumer);
+        TitaniumShapedRecipeBuilder.shapedRecipe(LINKING_TOOL.get())
+                .pattern("PPG").pattern("PDG").pattern("PEP")
+                .define('P', Items.PAPER)
+                .define('G', Tags.Items.INGOTS_GOLD)
+                .define('D', StorageTags.DRAWER)
+                .define('E', Items.DIAMOND)
+                .save(consumer);
+        TitaniumShapedRecipeBuilder.shapedRecipe(STORAGE_UPGRADES.get(StorageUpgradeItem.StorageTier.COPPER).get())
+                .pattern("IBI").pattern("CDC").pattern("IBI")
+                .define('I', Items.COPPER_INGOT)
+                .define('B', Items.COPPER_BLOCK)
+                .define('C', Tags.Items.CHESTS_WOODEN)
+                .define('D', StorageTags.DRAWER)
+                .save(consumer);
+        TitaniumShapedRecipeBuilder.shapedRecipe(STORAGE_UPGRADES.get(StorageUpgradeItem.StorageTier.GOLD).get())
+                .pattern("IBI").pattern("CDC").pattern("BIB")
+                .define('I', Tags.Items.INGOTS_GOLD)
+                .define('B', Tags.Items.STORAGE_BLOCKS_GOLD)
+                .define('C', Tags.Items.CHESTS_WOODEN)
+                .define('D', STORAGE_UPGRADES.get(StorageUpgradeItem.StorageTier.COPPER).get())
+                .save(consumer);
+        TitaniumShapedRecipeBuilder.shapedRecipe(STORAGE_UPGRADES.get(StorageUpgradeItem.StorageTier.DIAMOND).get())
+                .pattern("IBI").pattern("CDC").pattern("IBI")
+                .define('I', Tags.Items.GEMS_DIAMOND)
+                .define('B', Tags.Items.STORAGE_BLOCKS_DIAMOND)
+                .define('C', Tags.Items.CHESTS_WOODEN)
+                .define('D', STORAGE_UPGRADES.get(StorageUpgradeItem.StorageTier.GOLD).get())
+                .save(consumer);
+        TitaniumShapedRecipeBuilder.shapedRecipe(REDSTONE_UPGRADE.get())
+                .pattern("IBI").pattern("CDC").pattern("IBI")
+                .define('I', Items.REDSTONE)
+                .define('B', Items.REDSTONE_BLOCK)
+                .define('C', Items.COMPARATOR)
+                .define('D', StorageTags.DRAWER)
+                .save(consumer);
+        UpgradeRecipeBuilder.smithing(Ingredient.of(STORAGE_UPGRADES.get(StorageUpgradeItem.StorageTier.DIAMOND).get()), Ingredient.of(Items.NETHERITE_INGOT), STORAGE_UPGRADES.get(StorageUpgradeItem.StorageTier.NETHERITE).get())
+                .unlocks("has_netherite_ingot", has(Items.NETHERITE_INGOT))
+                .save(consumer, ForgeRegistries.ITEMS.getKey(STORAGE_UPGRADES.get(StorageUpgradeItem.StorageTier.NETHERITE).get()));
+        TitaniumShapedRecipeBuilder.shapedRecipe(ARMORY_CABINET.getLeft().get())
+                .pattern("ICI").pattern("CDC").pattern("IBI")
+                .define('I', Tags.Items.STONE)
+                .define('B', Tags.Items.INGOTS_NETHERITE)
+                .define('C', StorageTags.DRAWER)
+                .define('D', Items.COMPARATOR)
+                .save(consumer);
+        TitaniumShapedRecipeBuilder.shapedRecipe(PULLING_UPGRADE.get())
+                .pattern("ICI").pattern("IDI").pattern("IBI")
+                .define('I', Tags.Items.STONE)
+                .define('B', Tags.Items.DUSTS_REDSTONE)
+                .define('C', Items.HOPPER)
+                .define('D', StorageTags.DRAWER)
+                .save(consumer);
+        TitaniumShapedRecipeBuilder.shapedRecipe(PUSHING_UPGRADE.get())
+                .pattern("IBI").pattern("IDI").pattern("IRI")
+                .define('I', Tags.Items.STONE)
+                .define('B', Tags.Items.DUSTS_REDSTONE)
+                .define('R', Items.HOPPER)
+                .define('D', StorageTags.DRAWER)
+                .save(consumer);
+        TitaniumShapedRecipeBuilder.shapedRecipe(COLLECTOR_UPGRADE.get())
+                .pattern("IBI").pattern("RDR").pattern("IBI")
+                .define('I', Tags.Items.STONE)
+                .define('B', Items.HOPPER)
+                .define('R', Tags.Items.DUSTS_REDSTONE)
+                .define('D', StorageTags.DRAWER)
+                .save(consumer);
+        TitaniumShapedRecipeBuilder.shapedRecipe(ENDER_DRAWER.getLeft().get())
+                .pattern("PPP").pattern("LCL").pattern("PPP")
+                .define('P', ItemTags.PLANKS)
+                .define('C', Tags.Items.CHESTS_ENDER)
+                .define('L', StorageTags.DRAWER)
+                .save(consumer);
+    }
+}

src/main/java/com/buuz135/functionalstorage/util/CompactingUtil.java
@@ -1,5 +1,8 @@
 package com.buuz135.functionalstorage.util;
 
+import com.buuz135.functionalstorage.FunctionalStorage;
+import com.buuz135.functionalstorage.recipe.CustomCompactingRecipe;
+import com.hrznstudio.titanium.util.RecipeUtil;
 import net.minecraft.core.NonNullList;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
@@ -10,6 +13,7 @@ import net.minecraft.world.item.crafting.CraftingRecipe;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.items.ItemHandlerHelper;
 import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.*;
@@ -27,11 +31,13 @@ public class CompactingUtil {
     private final Level level;
     private List<Result> results;
     private final int resultAmount;
+    private final List<CustomCompactingRecipe> recipes;
 
     public CompactingUtil(Level level, int resultAmount) {
         this.level = level;
         this.resultAmount = resultAmount;
         this.results = new ArrayList<>();
+        this.recipes = (List<CustomCompactingRecipe>) RecipeUtil.getRecipes(level, FunctionalStorage.CUSTOM_COMPACTING_RECIPE_TYPE.get());
     }
 
     public void setup(ItemStack stack){
@@ -71,6 +77,11 @@ public class CompactingUtil {
     }
 
     private Result findUpperTier(ItemStack stack){
+        for (CustomCompactingRecipe recipe : this.recipes) {
+            if (ItemStack.isSame(recipe.lower_input, stack)) {
+                return new Result(ItemHandlerHelper.copyStackWithSize(recipe.higher_input, 1), recipe.lower_input.getCount());
+            }
+        }
         //Checking 3x3
         int sizeCheck = 9;
         CraftingContainer container = createContainerAndFill(3, stack);
@@ -108,6 +119,11 @@ public class CompactingUtil {
     }
 
     private Result findLowerTier(ItemStack stack){
+        for (CustomCompactingRecipe recipe : this.recipes) {
+            if (ItemStack.isSame(recipe.higher_input, stack)) {
+                return new Result(ItemHandlerHelper.copyStackWithSize(recipe.lower_input, 1), recipe.lower_input.getCount());
+            }
+        }
         List<ItemStack> candidates = new ArrayList<>();
         Map<ItemStack, Integer> candidatesRate = new HashMap<>();
         for (CraftingRecipe craftingRecipe : level.getRecipeManager().getAllRecipesFor(RecipeType.CRAFTING)) {
@@ -131,12 +147,13 @@ public class CompactingUtil {
                 }
             }
         }
+
         ItemStack similar = findSimilar(stack, candidates);
         if (!similar.isEmpty()){
             return new Result(similar, candidatesRate.get(similar));
         }
-        if (candidates.size() > 0){
-            return new Result(candidates.get(0), candidatesRate.get(candidates.get(0)));
+        if (!candidates.isEmpty()) {
+            return new Result(candidates.get(0).copy(), candidatesRate.get(candidates.get(0)));
         }
         return new Result(ItemStack.EMPTY, 0);
     }

coding-1ab/modpack-season-2

mods/FunctionalStorage/src/main/java/com/buuz135/functionalstorage/data/FunctionalStorageRecipesProvider.java
@@ -1,0 +1,127 @@
+package com.buuz135.functionalstorage.data;
+
+import com.buuz135.functionalstorage.item.StorageUpgradeItem;
+import com.buuz135.functionalstorage.util.StorageTags;
+import com.hrznstudio.titanium.block.BasicBlock;
+import com.hrznstudio.titanium.recipe.generator.TitaniumRecipeProvider;
+import com.hrznstudio.titanium.recipe.generator.TitaniumShapedRecipeBuilder;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.recipes.FinishedRecipe;
+import net.minecraft.data.recipes.RecipeCategory;
+import net.minecraft.data.recipes.SmithingTransformRecipeBuilder;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.common.Tags;
+import net.minecraftforge.common.util.NonNullLazy;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+import static com.buuz135.functionalstorage.FunctionalStorage.*;
+
+public class FunctionalStorageRecipesProvider extends TitaniumRecipeProvider {
+
+    private NonNullLazy<List<Block>> blocksToProcess;
+
+    public FunctionalStorageRecipesProvider(DataGenerator generator, NonNullLazy<List<Block>> blocksToProcess) {
+        super(generator);
+        this.blocksToProcess = blocksToProcess;
+    }
+
+    @Override
+    public void register(Consumer<FinishedRecipe> consumer) {
+        blocksToProcess.get().stream().map(block -> (BasicBlock) block).forEach(basicBlock -> basicBlock.registerRecipe(consumer));
+        TitaniumShapedRecipeBuilder.shapedRecipe(STORAGE_UPGRADES.get(StorageUpgradeItem.StorageTier.IRON).get())
+                .pattern("III").pattern("IDI").pattern("III")
+                .define('I', Tags.Items.INGOTS_IRON)
+                .define('D', StorageTags.DRAWER)
+                .save(consumer);
+        TitaniumShapedRecipeBuilder.shapedRecipe(VOID_UPGRADE.get())
+                .pattern("III").pattern("IDI").pattern("III")
+                .define('I', Tags.Items.OBSIDIAN)
+                .define('D', StorageTags.DRAWER)
+                .save(consumer);
+        TitaniumShapedRecipeBuilder.shapedRecipe(CONFIGURATION_TOOL.get())
+                .pattern("PPG").pattern("PDG").pattern("PEP")
+                .define('P', Items.PAPER)
+                .define('G', Tags.Items.INGOTS_GOLD)
+                .define('D', StorageTags.DRAWER)
+                .define('E', Items.EMERALD)
+                .save(consumer);
+        TitaniumShapedRecipeBuilder.shapedRecipe(LINKING_TOOL.get())
+                .pattern("PPG").pattern("PDG").pattern("PEP")
+                .define('P', Items.PAPER)
+                .define('G', Tags.Items.INGOTS_GOLD)
+                .define('D', StorageTags.DRAWER)
+                .define('E', Items.DIAMOND)
+                .save(consumer);
+        TitaniumShapedRecipeBuilder.shapedRecipe(STORAGE_UPGRADES.get(StorageUpgradeItem.StorageTier.COPPER).get())
+                .pattern("IBI").pattern("CDC").pattern("IBI")
+                .define('I', Items.COPPER_INGOT)
+                .define('B', Items.COPPER_BLOCK)
+                .define('C', Tags.Items.CHESTS_WOODEN)
+                .define('D', StorageTags.DRAWER)
+                .save(consumer);
+        TitaniumShapedRecipeBuilder.shapedRecipe(STORAGE_UPGRADES.get(StorageUpgradeItem.StorageTier.GOLD).get())
+                .pattern("IBI").pattern("CDC").pattern("BIB")
+                .define('I', Tags.Items.INGOTS_GOLD)
+                .define('B', Tags.Items.STORAGE_BLOCKS_GOLD)
+                .define('C', Tags.Items.CHESTS_WOODEN)
+                .define('D', STORAGE_UPGRADES.get(StorageUpgradeItem.StorageTier.COPPER).get())
+                .save(consumer);
+        TitaniumShapedRecipeBuilder.shapedRecipe(STORAGE_UPGRADES.get(StorageUpgradeItem.StorageTier.DIAMOND).get())
+                .pattern("IBI").pattern("CDC").pattern("IBI")
+                .define('I', Tags.Items.GEMS_DIAMOND)
+                .define('B', Tags.Items.STORAGE_BLOCKS_DIAMOND)
+                .define('C', Tags.Items.CHESTS_WOODEN)
+                .define('D', STORAGE_UPGRADES.get(StorageUpgradeItem.StorageTier.GOLD).get())
+                .save(consumer);
+        TitaniumShapedRecipeBuilder.shapedRecipe(REDSTONE_UPGRADE.get())
+                .pattern("IBI").pattern("CDC").pattern("IBI")
+                .define('I', Items.REDSTONE)
+                .define('B', Items.REDSTONE_BLOCK)
+                .define('C', Items.COMPARATOR)
+                .define('D', StorageTags.DRAWER)
+                .save(consumer);
+        SmithingTransformRecipeBuilder.smithing(Ingredient.of(Items.NETHERITE_UPGRADE_SMITHING_TEMPLATE), Ingredient.of(STORAGE_UPGRADES.get(StorageUpgradeItem.StorageTier.DIAMOND).get()), Ingredient.of(Items.NETHERITE_INGOT), RecipeCategory.MISC, STORAGE_UPGRADES.get(StorageUpgradeItem.StorageTier.NETHERITE).get())
+                .unlocks("has_netherite_ingot", has(Items.NETHERITE_INGOT))
+                .save(consumer, ForgeRegistries.ITEMS.getKey(STORAGE_UPGRADES.get(StorageUpgradeItem.StorageTier.NETHERITE).get()));
+        TitaniumShapedRecipeBuilder.shapedRecipe(ARMORY_CABINET.getLeft().get())
+                .pattern("ICI").pattern("CDC").pattern("IBI")
+                .define('I', Tags.Items.STONE)
+                .define('B', Tags.Items.INGOTS_NETHERITE)
+                .define('C', StorageTags.DRAWER)
+                .define('D', Items.COMPARATOR)
+                .save(consumer);
+        TitaniumShapedRecipeBuilder.shapedRecipe(PULLING_UPGRADE.get())
+                .pattern("ICI").pattern("IDI").pattern("IBI")
+                .define('I', Tags.Items.STONE)
+                .define('B', Tags.Items.DUSTS_REDSTONE)
+                .define('C', Items.HOPPER)
+                .define('D', StorageTags.DRAWER)
+                .save(consumer);
+        TitaniumShapedRecipeBuilder.shapedRecipe(PUSHING_UPGRADE.get())
+                .pattern("IBI").pattern("IDI").pattern("IRI")
+                .define('I', Tags.Items.STONE)
+                .define('B', Tags.Items.DUSTS_REDSTONE)
+                .define('R', Items.HOPPER)
+                .define('D', StorageTags.DRAWER)
+                .save(consumer);
+        TitaniumShapedRecipeBuilder.shapedRecipe(COLLECTOR_UPGRADE.get())
+                .pattern("IBI").pattern("RDR").pattern("IBI")
+                .define('I', Tags.Items.STONE)
+                .define('B', Items.HOPPER)
+                .define('R', Tags.Items.DUSTS_REDSTONE)
+                .define('D', StorageTags.DRAWER)
+                .save(consumer);
+        TitaniumShapedRecipeBuilder.shapedRecipe(ENDER_DRAWER.getLeft().get())
+                .pattern("PPP").pattern("LCL").pattern("PPP")
+                .define('P', ItemTags.PLANKS)
+                .define('C', Tags.Items.CHESTS_ENDER)
+                .define('L', StorageTags.DRAWER)
+                .save(consumer);
+    }
+}

mods/FunctionalStorage/src/main/java/com/buuz135/functionalstorage/util/CompactingUtil.java
@@ -1,5 +1,8 @@
 package com.buuz135.functionalstorage.util;
 
+import com.buuz135.functionalstorage.FunctionalStorage;
+import com.buuz135.functionalstorage.recipe.CustomCompactingRecipe;
+import com.hrznstudio.titanium.util.RecipeUtil;
 import net.minecraft.core.NonNullList;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
@@ -11,6 +14,7 @@ import net.minecraft.world.item.crafting.CraftingRecipe;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.items.ItemHandlerHelper;
 import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.*;
@@ -28,11 +32,13 @@ public class CompactingUtil {
     private final Level level;
     private List<Result> results;
     private final int resultAmount;
+    private final List<CustomCompactingRecipe> recipes;
 
     public CompactingUtil(Level level, int resultAmount) {
         this.level = level;
         this.resultAmount = resultAmount;
         this.results = new ArrayList<>();
+        this.recipes = (List<CustomCompactingRecipe>) RecipeUtil.getRecipes(level, FunctionalStorage.CUSTOM_COMPACTING_RECIPE_TYPE.get());
     }
 
     public void setup(ItemStack stack){
@@ -72,6 +78,11 @@ public class CompactingUtil {
     }
 
     private Result findUpperTier(ItemStack stack){
+        for (CustomCompactingRecipe recipe : this.recipes) {
+            if (ItemStack.isSameItem(recipe.lower_input, stack)) {
+                return new Result(ItemHandlerHelper.copyStackWithSize(recipe.higher_input, 1), recipe.lower_input.getCount());
+            }
+        }
         //Checking 3x3
         int sizeCheck = 9;
         CraftingContainer container = createContainerAndFill(3, stack);
@@ -109,6 +120,11 @@ public class CompactingUtil {
     }
 
     private Result findLowerTier(ItemStack stack){
+        for (CustomCompactingRecipe recipe : this.recipes) {
+            if (ItemStack.isSameItem(recipe.higher_input, stack)) {
+                return new Result(ItemHandlerHelper.copyStackWithSize(recipe.lower_input, 1), recipe.lower_input.getCount());
+            }
+        }
         List<ItemStack> candidates = new ArrayList<>();
         Map<ItemStack, Integer> candidatesRate = new HashMap<>();
         for (CraftingRecipe craftingRecipe : level.getRecipeManager().getAllRecipesFor(RecipeType.CRAFTING)) {
@@ -132,12 +148,13 @@ public class CompactingUtil {
                 }
             }
         }
+
         ItemStack similar = findSimilar(stack, candidates);
         if (!similar.isEmpty()){
             return new Result(similar, candidatesRate.get(similar));
         }
-        if (candidates.size() > 0){
-            return new Result(candidates.get(0), candidatesRate.get(candidates.get(0)));
+        if (!candidates.isEmpty()) {
+            return new Result(candidates.get(0).copy(), candidatesRate.get(candidates.get(0)));
         }
         return new Result(ItemStack.EMPTY, 0);
     }