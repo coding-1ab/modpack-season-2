3ef2743abea5a4b4d6c0129974ff3e02bb42b65f
simibubi <31564874+simibubi@users.noreply.github.com>
2023-11-12 12:39:15 +0100

    Bump version

--- mods/Create/src/main/java/com/simibubi/create/Create.java
+++ mods/Create/src/main/java/com/simibubi/create/Create.java
@@ -58,7 +58,7 @@ public class Create {
 
 	public static final String ID = "create";
 	public static final String NAME = "Create";
-	public static final String VERSION = "0.5.1f";
+	public static final String VERSION = "0.6-experimental";
 
 	public static final Logger LOGGER = LogUtils.getLogger();
 
