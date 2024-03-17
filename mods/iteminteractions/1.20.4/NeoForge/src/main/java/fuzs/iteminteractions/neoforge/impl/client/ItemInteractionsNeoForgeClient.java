a4fe859bbadb251ce2b2247e16fcd9011bef40eb
Fuzss <28218241+Fuzss@users.noreply.github.com>
2024-03-17 09:13:03 +0100

    add zh_cn.json

--- mods/iteminteractions/1.20.4/NeoForge/src/main/java/fuzs/iteminteractions/neoforge/impl/client/ItemInteractionsNeoForgeClient.java
+++ mods/iteminteractions/1.20.4/NeoForge/src/main/java/fuzs/iteminteractions/neoforge/impl/client/ItemInteractionsNeoForgeClient.java
@@ -2,7 +2,9 @@ package fuzs.iteminteractions.neoforge.impl.client;
 
 import fuzs.iteminteractions.impl.ItemInteractions;
 import fuzs.iteminteractions.impl.client.ItemInteractionsClient;
+import fuzs.iteminteractions.impl.data.client.ModLanguageProvider;
 import fuzs.puzzleslib.api.client.core.v1.ClientModConstructor;
+import fuzs.puzzleslib.neoforge.api.data.v2.core.DataProviderHelper;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
@@ -14,5 +16,6 @@ public class ItemInteractionsNeoForgeClient {
     @SubscribeEvent
     public static void onConstructMod(final FMLConstructModEvent evt) {
         ClientModConstructor.construct(ItemInteractions.MOD_ID, ItemInteractionsClient::new);
+        DataProviderHelper.registerDataProviders(ItemInteractions.MOD_ID, ModLanguageProvider::new);
     }
 }
