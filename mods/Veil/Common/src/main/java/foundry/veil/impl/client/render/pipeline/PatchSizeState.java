2feec620821ee8420ec8cd50702f550c460c7bca
Ocelot <FinntheRaider@gmail.com>
2024-04-07 23:42:48 -0600

    Update PatchSizeState.java

--- mods/Veil/Common/src/main/java/foundry/veil/impl/client/render/pipeline/PatchSizeState.java
+++ mods/Veil/Common/src/main/java/foundry/veil/impl/client/render/pipeline/PatchSizeState.java
@@ -2,15 +2,15 @@ package foundry.veil.impl.client.render.pipeline;
 
 import net.minecraft.client.renderer.RenderStateShard;
 import org.jetbrains.annotations.ApiStatus;
-import org.lwjgl.opengl.GL40C;
 
+import static org.lwjgl.opengl.GL40C.GL_PATCH_VERTICES;
 import static org.lwjgl.opengl.GL40C.glPatchParameteri;
 
 @ApiStatus.Internal
 public class PatchSizeState extends RenderStateShard {
 
     public PatchSizeState(int patchVertices) {
-        super("patch", () -> glPatchParameteri(GL40C.GL_PATCH_VERTICES, patchVertices), () -> {
+        super("patch", () -> glPatchParameteri(GL_PATCH_VERTICES, patchVertices), () -> {
         });
     }
 }
