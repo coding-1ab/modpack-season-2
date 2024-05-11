3d1ec07f4ef26be5bab305df9641d022c5130a66
Ocelot <FinntheRaider@gmail.com>
2024-05-11 14:08:21 -0600

    Fixed missing shader error

--- mods/Veil/Common/src/main/java/foundry/veil/mixin/client/pipeline/RenderSystemMixin.java
+++ mods/Veil/Common/src/main/java/foundry/veil/mixin/client/pipeline/RenderSystemMixin.java
@@ -2,6 +2,7 @@ package foundry.veil.mixin.client.pipeline;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import foundry.veil.api.client.render.VeilRenderSystem;
+import foundry.veil.impl.client.render.shader.ShaderProgramImpl;
 import net.minecraft.client.renderer.ShaderInstance;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,6 +16,8 @@ public class RenderSystemMixin {
 
     @Inject(method = "setShader", at = @At("TAIL"), remap = false)
     private static void setShader(Supplier<ShaderInstance> shader, CallbackInfo ci) {
-        VeilRenderSystem.shaderUpdate();
+        if (!(shader.get() instanceof ShaderProgramImpl.Wrapper)) {
+            VeilRenderSystem.shaderUpdate();
+        }
     }
 }
