5806598c00499b3ec6dc675821f5cd0fa15052eb
Ocelot <FinntheRaider@gmail.com>
2024-06-04 17:33:25 -0600

    Fixed compile error

--- mods/Veil/Common/src/main/java/foundry/veil/api/client/necromancer/Mesh.java
+++ mods/Veil/Common/src/main/java/foundry/veil/api/client/necromancer/Mesh.java
@@ -4,5 +4,6 @@ import com.mojang.blaze3d.vertex.BufferBuilder;
 
 // todo: make this more generic? just as a mesh class for everyone to use in veil.
 public interface Mesh {
-    default BufferBuilder.RenderedBuffer createMesh() {}
+
+    BufferBuilder.RenderedBuffer createMesh();
 }
