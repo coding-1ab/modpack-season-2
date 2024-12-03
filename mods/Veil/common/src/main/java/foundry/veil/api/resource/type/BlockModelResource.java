c1933a833bf24d7e9acde60221ae4a8b8f408ac3
Ocelot <FinntheRaider@gmail.com>
2024-12-02 19:42:07 -0700

    Update BlockModelResource.java

--- mods/Veil/common/src/main/java/foundry/veil/api/resource/type/BlockModelResource.java
+++ mods/Veil/common/src/main/java/foundry/veil/api/resource/type/BlockModelResource.java
@@ -2,27 +2,30 @@ package foundry.veil.api.resource.type;
 
 import foundry.veil.Veil;
 import foundry.veil.api.client.render.VeilRenderSystem;
-import foundry.veil.api.resource.VeilResource;
 import foundry.veil.api.resource.VeilResourceAction;
 import foundry.veil.api.resource.VeilResourceInfo;
 import foundry.veil.api.resource.VeilResourceManager;
 import foundry.veil.impl.resource.action.ModelEditAction;
+import foundry.veil.impl.resource.action.TextEditAction;
+import imgui.extension.texteditor.TextEditorLanguageDefinition;
 import net.minecraft.Util;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.model.ModelManager;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.util.profiling.ProfilerFiller;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
 
-public record BlockModelResource(VeilResourceInfo resourceInfo) implements VeilResource<BlockModelResource> {
+public record BlockModelResource(VeilResourceInfo resourceInfo) implements VeilTextResource<BlockModelResource> {
 
     @Override
     public List<VeilResourceAction<BlockModelResource>> getActions() {
-        return List.of(new ModelEditAction<>());
+        return List.of(new TextEditAction<>(), new ModelEditAction<>());
     }
 
     @Override
@@ -35,20 +38,19 @@ public record BlockModelResource(VeilResourceInfo resourceInfo) implements VeilR
         ResourceLocation location = this.resourceInfo.location();
         ResourceManager resources = resourceManager.resources(this.resourceInfo);
         Minecraft client = Minecraft.getInstance();
-
         ModelManager modelManager = client.getModelManager();
+        ProfilerFiller profiler = client.getProfiler();
 
-        try {
-            ProfilerFiller profiler = client.getProfiler();
-
-            // TODO: Potentially look at more targeted model reload?
-            CompletableFuture.allOf(modelManager.reload(CompletableFuture::completedFuture, resources, profiler, profiler, Util.backgroundExecutor(), client)).handle((u, t) -> {
-                Minecraft.getInstance().tell(VeilRenderSystem::rebuildChunks);
-                return null;
-            });
-        } catch (Exception exception) {
-            Veil.LOGGER.error("Failed to load model {}", location, exception);
-        }
+        // TODO: Potentially look at more targeted model reload?
+        modelManager.reload(CompletableFuture::completedFuture, resources, profiler, profiler, Util.backgroundExecutor(), client)
+                .thenAcceptAsync(unused -> VeilRenderSystem.rebuildChunks(), client)
+                .exceptionally(e -> {
+                    while (e instanceof CompletionException) {
+                        e = e.getCause();
+                    }
+                    Veil.LOGGER.error("Failed to load model {}", location, e);
+                    return null;
+                });
     }
 
     @Override
@@ -56,4 +58,8 @@ public record BlockModelResource(VeilResourceInfo resourceInfo) implements VeilR
         return 0xF383;
     }
 
+    @Override
+    public @Nullable TextEditorLanguageDefinition languageDefinition() {
+        return null;
+    }
 }
