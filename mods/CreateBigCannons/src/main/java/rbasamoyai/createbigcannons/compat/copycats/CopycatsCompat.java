8b934c93d765ac9083395a0dcffc65cd1fd71830
realRobotix <82544307+realRobotix@users.noreply.github.com>
2024-12-08 16:55:30 +0100

    dependency, improvement: add new copycat blocks to compat

--- mods/CreateBigCannons/src/main/java/rbasamoyai/createbigcannons/compat/copycats/CopycatsCompat.java
+++ mods/CreateBigCannons/src/main/java/rbasamoyai/createbigcannons/compat/copycats/CopycatsCompat.java
@@ -11,28 +11,36 @@ import rbasamoyai.createbigcannons.block_armor_properties.mimicking_blocks.Copyc
 public class CopycatsCompat {
 
 	public static void init() {
-		registerCopycatSerializer(CCBlocks.COPYCAT_BEAM.get());
 		registerCopycatSerializer(CCBlocks.COPYCAT_BLOCK.get());
+		registerCopycatSerializer(CCBlocks.COPYCAT_BEAM.get());
 		registerCopycatSerializer(CCBlocks.COPYCAT_BOARD.get());
+		registerCopycatSerializer(CCBlocks.COPYCAT_WOODEN_BUTTON.get());
+		registerCopycatSerializer(CCBlocks.COPYCAT_STONE_BUTTON.get());
 		registerCopycatSerializer(CCBlocks.COPYCAT_BYTE.get());
 		registerCopycatSerializer(CCBlocks.COPYCAT_FENCE.get());
 		registerCopycatSerializer(CCBlocks.COPYCAT_FENCE_GATE.get());
 		registerCopycatSerializer(CCBlocks.COPYCAT_HALF_LAYER.get());
 		registerCopycatSerializer(CCBlocks.COPYCAT_HALF_PANEL.get());
-		registerCopycatSerializer(CCBlocks.COPYCAT_HEAVY_WEIGHTED_PRESSURE_PLATE.get());
+		registerCopycatSerializer(CCBlocks.COPYCAT_LADDER.get());
 		registerCopycatSerializer(CCBlocks.COPYCAT_LAYER.get());
+		registerCopycatSerializer(CCBlocks.COPYCAT_WOODEN_PRESSURE_PLATE.get());
+		registerCopycatSerializer(CCBlocks.COPYCAT_STONE_PRESSURE_PLATE.get());
+		registerCopycatSerializer(CCBlocks.COPYCAT_HEAVY_WEIGHTED_PRESSURE_PLATE.get());
 		registerCopycatSerializer(CCBlocks.COPYCAT_LIGHT_WEIGHTED_PRESSURE_PLATE.get());
 		registerCopycatSerializer(CCBlocks.COPYCAT_SLAB.get());
 		registerCopycatSerializer(CCBlocks.COPYCAT_SLICE.get());
 		registerCopycatSerializer(CCBlocks.COPYCAT_STAIRS.get());
-		registerCopycatSerializer(CCBlocks.COPYCAT_STONE_BUTTON.get());
-		registerCopycatSerializer(CCBlocks.COPYCAT_STONE_PRESSURE_PLATE.get());
+		registerCopycatSerializer(CCBlocks.COPYCAT_VERTICAL_STAIRS.get());
 		registerCopycatSerializer(CCBlocks.COPYCAT_TRAPDOOR.get());
+		registerCopycatSerializer(CCBlocks.COPYCAT_IRON_TRAPDOOR.get());
 		registerCopycatSerializer(CCBlocks.COPYCAT_VERTICAL_SLICE.get());
 		registerCopycatSerializer(CCBlocks.COPYCAT_VERTICAL_STEP.get());
 		registerCopycatSerializer(CCBlocks.COPYCAT_WALL.get());
-		registerCopycatSerializer(CCBlocks.COPYCAT_WOODEN_BUTTON.get());
-		registerCopycatSerializer(CCBlocks.COPYCAT_WOODEN_PRESSURE_PLATE.get());
+		registerCopycatSerializer(CCBlocks.COPYCAT_SLOPE.get());
+		registerCopycatSerializer(CCBlocks.COPYCAT_VERTICAL_SLOPE.get());
+		registerCopycatSerializer(CCBlocks.COPYCAT_SLOPE_LAYER.get());
+		registerCopycatSerializer(CCBlocks.COPYCAT_DOOR.get());
+		registerCopycatSerializer(CCBlocks.COPYCAT_IRON_DOOR.get());
 
 	}
 
