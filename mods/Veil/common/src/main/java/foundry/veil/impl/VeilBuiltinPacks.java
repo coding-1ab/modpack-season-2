1a2b5389a36d3528e19f91898d7d14862eb9d779
Ocelot <FinntheRaider@gmail.com>
2025-01-06 20:56:11 -0700

    Moved fog post to resource pack

--- mods/Veil/common/src/main/java/foundry/veil/impl/VeilBuiltinPacks.java
+++ mods/Veil/common/src/main/java/foundry/veil/impl/VeilBuiltinPacks.java
@@ -11,7 +11,7 @@ public class VeilBuiltinPacks {
         if (Veil.platform().isDevelopmentEnvironment()) {
             context.register(Veil.veilPath("test_particles"), false);
             context.register(Veil.veilPath("test_shaders"), false);
-            context.register(Veil.veilPath("volumetric_fog"), false);
+            context.register(Veil.veilPath("fog"), false);
         }
 
 //        if (!Veil.SODIUM) {
