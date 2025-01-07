85de60803dec97688c431d58fa4bc205289a595a
IThundxr <me@ithundxr.dev>
2025-01-07 16:57:08 -0500

    Gaslight by the loader
    - Fix ArmorTrimMixin, lambda names are different in prod for some reason, and are too brittle to even try relying on

--- mods/Create/src/main/java/com/simibubi/create/foundation/mixin/ArmorTrimMixin.java
+++ mods/Create/src/main/java/com/simibubi/create/foundation/mixin/ArmorTrimMixin.java
@@ -1,23 +1,53 @@
 package com.simibubi.create.foundation.mixin;
 
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
+import java.util.function.BiFunction;
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import com.llamalad7.mixinextras.sugar.Local;
 import com.simibubi.create.Create;
+
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
 import com.simibubi.create.content.equipment.armor.AllArmorMaterials;
 
+import net.minecraft.Util;
+import net.minecraft.core.Holder;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.armortrim.ArmorTrim;
+import net.minecraft.world.item.armortrim.TrimPattern;
 
 @Mixin(ArmorTrim.class)
-public class ArmorTrimMixin {
-	@ModifyExpressionValue(method = {"lambda$new$2", "lambda$new$4"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/armortrim/TrimPattern;assetId()Lnet/minecraft/resources/ResourceLocation;"))
-	private ResourceLocation create$swapTexturesForCardboardTrims(ResourceLocation original, @Local(argsOnly = true) ArmorMaterial armorMaterial) {
-		if (armorMaterial == AllArmorMaterials.CARDBOARD)
-			return Create.asResource("card_" + original.getPath());
-		return original;
+public abstract class ArmorTrimMixin {
+	@Shadow
+	@Final
+	private Holder<TrimPattern> pattern;
+
+	@Shadow
+	protected abstract String getColorPaletteSuffix(ArmorMaterial pArmorMaterial);
+
+	@Unique
+	private final BiFunction<Boolean, ArmorMaterial, ResourceLocation> create$textureCardboard = Util.memoize((inner, material) -> {
+		String assetPath = pattern.value().assetId().getPath();
+		String colorSuffix = getColorPaletteSuffix(material);
+		return Create.asResource("trims/models/armor/" + assetPath + (inner ? "_leggings_" : "_") + colorSuffix);
+	});
+
+	@Inject(method = "innerTexture", at = @At("HEAD"), cancellable = true)
+	private void create$swapTexturesForCardboardTrimsInner(ArmorMaterial pArmorMaterial, CallbackInfoReturnable<ResourceLocation> cir) {
+		if (pArmorMaterial == AllArmorMaterials.CARDBOARD) {
+			cir.setReturnValue(create$textureCardboard.apply(true, pArmorMaterial));
+		}
+	}
+
+	@Inject(method = "outerTexture", at = @At("HEAD"), cancellable = true)
+	private void create$swapTexturesForCardboardTrimsOuter(ArmorMaterial pArmorMaterial, CallbackInfoReturnable<ResourceLocation> cir) {
+		if (pArmorMaterial == AllArmorMaterials.CARDBOARD) {
+			cir.setReturnValue(create$textureCardboard.apply(true, pArmorMaterial));
+		}
 	}
 }
