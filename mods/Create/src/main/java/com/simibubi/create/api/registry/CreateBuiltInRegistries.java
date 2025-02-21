7807ed21ad965471cad9f4093787969a83946d75
IThundxr <me@ithundxr.dev>
2025-02-21 16:42:07 -0500

    CoreModn't

--- mods/Create/src/main/java/com/simibubi/create/api/registry/CreateBuiltInRegistries.java
+++ mods/Create/src/main/java/com/simibubi/create/api/registry/CreateBuiltInRegistries.java
@@ -1,7 +1,5 @@
 package com.simibubi.create.api.registry;
 
-import java.lang.reflect.Field;
-
 import org.jetbrains.annotations.ApiStatus;
 
 import com.mojang.serialization.Lifecycle;
@@ -13,11 +11,11 @@ import com.simibubi.create.api.contraption.storage.item.MountedItemStorageType;
 import com.simibubi.create.content.kinetics.fan.processing.FanProcessingType;
 import com.simibubi.create.content.kinetics.mechanicalArm.ArmInteractionPointType;
 import com.simibubi.create.content.logistics.item.filter.attribute.ItemAttributeType;
+import com.simibubi.create.foundation.mixin.accessor.BuiltInRegistriesAccessor;
 
 import net.minecraft.core.MappedRegistry;
 import net.minecraft.core.Registry;
 import net.minecraft.core.WritableRegistry;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceKey;
 
 /**
@@ -26,8 +24,6 @@ import net.minecraft.resources.ResourceKey;
  * @see CreateRegistries
  */
 public class CreateBuiltInRegistries {
-	private static final WritableRegistry<WritableRegistry<?>> ROOT_REGISTRY = getRootRegistry();
-
 	public static final Registry<ArmInteractionPointType> ARM_INTERACTION_POINT_TYPE = simple(CreateRegistries.ARM_INTERACTION_POINT_TYPE);
 	public static final Registry<FanProcessingType> FAN_PROCESSING_TYPE = simple(CreateRegistries.FAN_PROCESSING_TYPE);
 	public static final Registry<ItemAttributeType> ITEM_ATTRIBUTE_TYPE = simple(CreateRegistries.ITEM_ATTRIBUTE_TYPE);
@@ -47,27 +43,15 @@ public class CreateBuiltInRegistries {
 
 	@SuppressWarnings("unchecked")
 	private static <T> Registry<T> register(ResourceKey<Registry<T>> key, WritableRegistry<T> registry) {
-		ROOT_REGISTRY.register(
+		BuiltInRegistriesAccessor.create$getWRITABLE_REGISTRY().register(
 			(ResourceKey<WritableRegistry<?>>) (Object) key, registry, Lifecycle.stable()
 		);
 		return registry;
 	}
 
-	@SuppressWarnings("unchecked")
-	private static WritableRegistry<WritableRegistry<?>> getRootRegistry() {
-		// an accessor can't be used here because BuiltInRegistries is loaded too early during datagen.
-		try {
-			Field field = BuiltInRegistries.class.getDeclaredField("WRITABLE_REGISTRY");
-			field.setAccessible(true);
-			return (WritableRegistry<WritableRegistry<?>>) field.get(null);
-		} catch (NoSuchFieldException | IllegalAccessException e) {
-			throw new RuntimeException("Create: Failed to get root registry", e);
-		}
-	}
-
 	@ApiStatus.Internal
 	public static void init() {
 		// make sure the class is loaded.
-		// this method is called at the tail of BuiltInRegistries, injected by a coremod. See it for details.
+		// this method is called at the tail of BuiltInRegistries, injected by BuiltInRegistriesMixin.
 	}
 }
