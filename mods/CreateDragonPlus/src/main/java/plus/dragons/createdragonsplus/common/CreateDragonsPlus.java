bd25bb1328c6f9057a7226910be0aa57bc1cb164
RaymondBlaze <raymond-blaze@outlook.com>
2025-03-01 17:04:14 +0800

    [cleanup] Filled in template project

---
+++ mods/CreateDragonPlus/src/main/java/plus/dragons/createdragonsplus/common/CreateDragonsPlus.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2025  DragonsPlus
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package plus.dragons.createdragonsplus.common;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+
+@Mod(CreateDragonsPlus.ID)
+public class CreateDragonsPlus {
+    public static final String ID = "create_dragons_plus";
+
+    public CreateDragonsPlus(IEventBus modBus) {
+        modBus.register(this);
+    }
+
+    public void setup(final FMLCommonSetupEvent event) {}
+}
