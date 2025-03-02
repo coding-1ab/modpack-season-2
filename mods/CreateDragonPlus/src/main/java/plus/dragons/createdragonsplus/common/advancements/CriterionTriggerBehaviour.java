a5c98618ca8e7a48e5db066b3069efd2fe8bb89a
RaymondBlaze <raymond-blaze@outlook.com>
2025-03-02 16:38:44 +0800

    [fix] Improved CriterionTriggerBehaviour

--- mods/CreateDragonPlus/src/main/java/plus/dragons/createdragonsplus/common/advancements/CriterionTriggerBehaviour.java
+++ mods/CreateDragonPlus/src/main/java/plus/dragons/createdragonsplus/common/advancements/CriterionTriggerBehaviour.java
@@ -18,6 +18,7 @@
 
 package plus.dragons.createdragonsplus.common.advancements;
 
+import com.google.common.collect.ImmutableMap;
 import com.mojang.logging.LogUtils;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
@@ -31,6 +32,7 @@ import java.util.Optional;
 import java.util.UUID;
 import net.minecraft.core.HolderLookup.Provider;
 import net.minecraft.core.UUIDUtil;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.NbtOps;
 import net.minecraft.nbt.Tag;
@@ -39,13 +41,14 @@ import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import plus.dragons.createdragonsplus.common.CreateDragonsPlus;
+import plus.dragons.createdragonsplus.common.advancements.criterion.BlockEntityBehaviourTrigger;
 import plus.dragons.createdragonsplus.util.ErrorMessages;
 
 public class CriterionTriggerBehaviour extends BlockEntityBehaviour {
     public static final BehaviourType<CriterionTriggerBehaviour> TYPE =
             new BehaviourType<>(CreateDragonsPlus.asResource("criterion").toString());
     private static final Logger LOGGER = LogUtils.getLogger();
-    private static final MapCodec<UUID> UUID_CODEC = UUIDUtil.CODEC.optionalFieldOf("owner", null);
+    private static final MapCodec<UUID> OWNER_CODEC = UUIDUtil.CODEC.optionalFieldOf("owner", null);
     private static final MapCodec<Map<BlockEntityBehaviourTrigger<?>, Object>> DATA_CODEC =
             Codec.<BlockEntityBehaviourTrigger<?>, Object>dispatchedMap(
                             BlockEntityBehaviourTrigger.CODEC, BlockEntityBehaviourTrigger::dataCodec)
@@ -53,43 +56,59 @@ public class CriterionTriggerBehaviour extends BlockEntityBehaviour {
                     .xmap(
                             optional -> optional.orElse(new IdentityHashMap<>()),
                             map -> map.isEmpty() ? Optional.empty() : Optional.of(map));
-    protected @Nullable UUID ownerId;
+    protected @Nullable UUID owner;
+    protected final Map<BlockEntityBehaviourTrigger<?>, Object> defaultData;
     protected Map<BlockEntityBehaviourTrigger<?>, Object> data;
 
-    public CriterionTriggerBehaviour(SmartBlockEntity be) {
+    protected CriterionTriggerBehaviour(SmartBlockEntity be, Map<BlockEntityBehaviourTrigger<?>, Object> defaultData) {
         super(be);
-        this.data = new IdentityHashMap<>();
+        this.defaultData = ImmutableMap.copyOf(defaultData);
+        this.data = new IdentityHashMap<>(defaultData);
     }
 
-    public void setOwner(@Nullable UUID ownerId) {
-        this.ownerId = ownerId;
+    public static Builder builder(SmartBlockEntity blockEntity) {
+        return new Builder(blockEntity);
+    }
+
+    public void setOwner(@Nullable UUID owner) {
+        if (this.owner != owner) {
+            this.owner = owner;
+            this.data = new IdentityHashMap<>(this.defaultData);
+            this.blockEntity.setChanged();
+        }
     }
 
     public void setOwner(ServerPlayer player) {
-        this.ownerId = player.getUUID();
+        this.owner = player.getUUID();
+        this.setOwner(player.getUUID());
     }
 
     @Nullable
     public ServerPlayer getOwner() {
-        if (this.ownerId == null) return null;
+        if (this.owner == null) return null;
         Level level = this.getWorld();
         if (level.isClientSide) return null;
-        if (level.getPlayerByUUID(this.ownerId) instanceof ServerPlayer player) return player;
+        if (level.getPlayerByUUID(this.owner) instanceof ServerPlayer player) return player;
         return null;
     }
 
     @SuppressWarnings("unchecked")
     @Nullable
     public <T> T getData(BlockEntityBehaviourTrigger<T> trigger) {
-        return (T) data.get(trigger);
+        if (data.containsKey(trigger))
+            return (T) data.get(trigger);
+        return (T) this.defaultData.get(trigger);
     }
 
     public <T> void setData(BlockEntityBehaviourTrigger<T> trigger, T data) {
+        if (!this.defaultData.containsKey(trigger)) {
+            LOGGER.warn("Attempted to set data for unregistered trigger [" +
+                        ErrorMessages.registry(BuiltInRegistries.TRIGGER_TYPES, trigger) +
+                        "]");
+            return;
+        }
         this.data.put(trigger, data);
-    }
-
-    protected void setData(Map<BlockEntityBehaviourTrigger<?>, Object> data) {
-        this.data = data;
+        this.blockEntity.setChanged();
     }
 
     @Override
@@ -102,7 +121,7 @@ public class CriterionTriggerBehaviour extends BlockEntityBehaviour {
         super.write(nbtIn, registries, clientPacket);
         if (clientPacket) return;
         var builder = NbtOps.INSTANCE.mapBuilder();
-        UUID_CODEC.encode(this.ownerId, NbtOps.INSTANCE, builder);
+        OWNER_CODEC.encode(this.owner, NbtOps.INSTANCE, builder);
         DATA_CODEC.encode(this.data, NbtOps.INSTANCE, builder);
         builder.build(new CompoundTag())
                 .resultOrPartial(error -> LOGGER.error(
@@ -117,7 +136,26 @@ public class CriterionTriggerBehaviour extends BlockEntityBehaviour {
         if (!nbtIn.contains(getType().getName())) return;
         CompoundTag nbt = nbtIn.getCompound(getType().getName());
         MapLike<Tag> mapLike = NbtOps.INSTANCE.getMap(nbt).getOrThrow();
-        UUID_CODEC.decode(NbtOps.INSTANCE, mapLike).ifSuccess(this::setOwner);
-        DATA_CODEC.decode(NbtOps.INSTANCE, mapLike).ifSuccess(this::setData);
+        OWNER_CODEC.decode(NbtOps.INSTANCE, mapLike).ifSuccess(id -> this.owner = id);
+        DATA_CODEC.decode(NbtOps.INSTANCE, mapLike).ifSuccess(data -> this.data = data);
+        this.data.keySet().removeIf(trigger -> !this.defaultData.containsKey(trigger));
+    }
+
+    public static class Builder {
+        private final SmartBlockEntity blockEntity;
+        private final Map<BlockEntityBehaviourTrigger<?>, Object> data = new IdentityHashMap<>();
+
+        private Builder(SmartBlockEntity blockEntity) {
+            this.blockEntity = blockEntity;
+        }
+
+        public <T> Builder add(BlockEntityBehaviourTrigger<T> trigger, T defaultValue) {
+            this.data.put(trigger, defaultValue);
+            return this;
+        }
+
+        public CriterionTriggerBehaviour build() {
+            return new CriterionTriggerBehaviour(this.blockEntity, this.data);
+        }
     }
 }
