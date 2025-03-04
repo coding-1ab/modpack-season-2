9cbb94a82d304e7614826de337a959daca76564e
IThundxr <me@ithundxr.dev>
2025-03-04 12:49:15 -0500

    Trimless
    - Fix diving armor being trimmable

--- mods/Create/src/main/java/com/simibubi/create/foundation/mixin/SmithingTrimRecipeMixin.java
+++ mods/Create/src/main/java/com/simibubi/create/foundation/mixin/SmithingTrimRecipeMixin.java
@@ -1,20 +1,24 @@
 package com.simibubi.create.foundation.mixin;
 
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
 import com.simibubi.create.AllTags.AllItemTags;
 
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.crafting.SmithingTrimRecipe;
+import net.neoforged.neoforge.common.crafting.DifferenceIngredient;
 
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.crafting.SmithingTrimRecipe;
 
 @Mixin(SmithingTrimRecipe.class)
 public class SmithingTrimRecipeMixin {
-	@Inject(method = "isBaseIngredient", at = @At("HEAD"), cancellable = true)
-	private void create$preventTrimmingDivingArmor(ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
-		if (stack.is(AllItemTags.DIVING_ARMOR.tag))
-			cir.setReturnValue(false);
+	@Unique
+	private static final Ingredient NON_TRIMMABLE_ARMOR = Ingredient.of(AllItemTags.DIVING_ARMOR.tag);
+
+	@ModifyVariable(method = "<init>", at = @At("CTOR_HEAD"), argsOnly = true, ordinal = 1)
+	private Ingredient create$preventTrimmingDivingArmor(Ingredient base) {
+		return new DifferenceIngredient(base, NON_TRIMMABLE_ARMOR).toVanilla();
 	}
 }
