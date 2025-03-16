311aa497a1d023e10bfc8939da4a24760bef5ac2
Ocelot <FinntheRaider@gmail.com>
2025-03-15 23:28:04 -0600

    Fixed sodium mixin error

--- mods/Veil/neoforge/src/main/java/foundry/veil/forge/mixin/client/perspective/sodium/SodiumWorldRendererMixin.java
+++ mods/Veil/neoforge/src/main/java/foundry/veil/forge/mixin/client/perspective/sodium/SodiumWorldRendererMixin.java
@@ -1,6 +1,5 @@
 package foundry.veil.forge.mixin.client.perspective.sodium;
 
-import foundry.veil.api.client.render.VeilLevelPerspectiveRenderer;
 import foundry.veil.forge.ext.SodiumWorldRendererExtension;
 import net.caffeinemc.mods.sodium.client.render.SodiumWorldRenderer;
 import net.caffeinemc.mods.sodium.client.render.chunk.ChunkUpdateType;
@@ -9,8 +8,6 @@ import net.caffeinemc.mods.sodium.client.render.chunk.RenderSectionManager;
 import net.caffeinemc.mods.sodium.client.render.chunk.lists.SortedRenderLists;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 import java.util.ArrayDeque;
 import java.util.Map;
@@ -20,11 +17,6 @@ public class SodiumWorldRendererMixin implements SodiumWorldRendererExtension {
 
     @Shadow(remap = false)
     private RenderSectionManager renderSectionManager;
-
-    @ModifyVariable(method = "setupTerrain", at = @At(value = "INVOKE", target = "Lnet/caffeinemc/mods/sodium/client/render/chunk/RenderSectionManager;updateCameraState(Lorg/joml/Vector3dc;Lnet/minecraft/client/Camera;)V"), ordinal = 2, remap = false)
-    public boolean setCameraLocationChanged(boolean value) {
-        return !VeilLevelPerspectiveRenderer.isRenderingPerspective() && value;
-    }
 
     @Override
     public SortedRenderLists veil$getSortedRenderLists() {
