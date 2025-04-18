b11f65e20ad68067ea9c50ab185ed644a3c72a77
MRH0 <mrhmail1@gmail.com>
2025-04-18 14:16:49 +0200

    fixed data

--- mods/createaddition/src/main/java/com/mrh0/createaddition/datagen/RecipeProvider/CALiquidBurningRecipeProvider.java
+++ mods/createaddition/src/main/java/com/mrh0/createaddition/datagen/RecipeProvider/CALiquidBurningRecipeProvider.java
@@ -14,6 +14,7 @@ import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.FluidTags;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Recipe;
@@ -59,7 +60,14 @@ public class CALiquidBurningRecipeProvider extends ProcessingRecipeGen {
     protected void buildRecipes(@NotNull RecipeOutput output) {
         CALiquidBurningRecipeBuilder.liquidBurning(24000)
                 .require(CATagRegister.Fluids.BIOFUEL)
+                .superheated()
                 .save(output, "biofuel");
+        CALiquidBurningRecipeBuilder.liquidBurning(4800)
+                .require(CATagRegister.Fluids.PLANTOIL)
+                .save(output, "plantoil");
+        CALiquidBurningRecipeBuilder.liquidBurning(20000)
+                .require(FluidTags.LAVA)
+                .save(output, "lava");
     }
 
     @Override
