4746ba0c8a9ef072206841f2688cb3b104a24394
banna12345bob <banna12345bob@outlook.com>
2025-05-01 17:29:26 +1000

    Started reimplementing ponders

--- mods/create-new-age/common/src/main/java/org/antarcticgardens/cna/CreateNewAgeClient.java
+++ mods/create-new-age/common/src/main/java/org/antarcticgardens/cna/CreateNewAgeClient.java
@@ -1,7 +1,9 @@
 package org.antarcticgardens.cna;
 
+import net.createmod.ponder.foundation.PonderIndex;
+
 public abstract class CreateNewAgeClient {
     protected void initialize() {
-        
+        PonderIndex.addPlugin(new CNAPonders());
     }
 }
