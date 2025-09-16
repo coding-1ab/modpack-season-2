16b0c8ca5bb4eefa954775de5fa0cf50aba0ef7c
IThundxr <me@ithundxr.dev>
2025-09-16 19:55:08 -0400

    Fix missing brackets

--- mods/Create/src/main/java/com/simibubi/create/content/kinetics/chainConveyor/ChainPackageInteractionPacket.java
+++ mods/Create/src/main/java/com/simibubi/create/content/kinetics/chainConveyor/ChainPackageInteractionPacket.java
@@ -72,19 +72,16 @@ public class ChainPackageInteractionPacket extends BlockEntityConfigurationPacke
 				best = liftPackage;
 			}
 
-			if (best == null)
-				return;
-
-			if (player.isHolding(stack -> stack.isEmpty())) {
+			if (player.isHolding(ItemStack::isEmpty)) {
 				if (player.getMainHandItem().isEmpty()) {
 					player.setItemInHand(InteractionHand.MAIN_HAND, best.item.copy());
 				} else {
 					player.setItemInHand(InteractionHand.OFF_HAND, best.item.copy());
 				}
-			}
-			else
+			} else {
 				player.getInventory()
 					.placeItemBackInInventory(best.item.copy());
+			}
 
 			list.remove(best);
 			be.sendData();
@@ -94,21 +91,24 @@ public class ChainPackageInteractionPacket extends BlockEntityConfigurationPacke
 				return;
 			}
 
-		if (!player.isCreative()) {
-			player.getMainHandItem().shrink(1);
-			player.getOffhandItem().shrink(1);
-			if (player.isHolding(stack -> stack.isEmpty())) { 
-				if (player.getMainHandItem().isEmpty()) {
-					player.setItemInHand(InteractionHand.MAIN_HAND, ItemStack.EMPTY);
-				} else {
-					player.setItemInHand(InteractionHand.OFF_HAND, ItemStack.EMPTY);
-				}
+			if (!player.isCreative()) {
+				player.getMainHandItem().shrink(1);
+				player.getOffhandItem().shrink(1);
+				if (player.isHolding(ItemStack::isEmpty)) {
+					if (player.getMainHandItem().isEmpty()) {
+						player.setItemInHand(InteractionHand.MAIN_HAND, ItemStack.EMPTY);
+					} else {
+						player.setItemInHand(InteractionHand.OFF_HAND, ItemStack.EMPTY);
+					}
 
-			if (selectedConnection.equals(BlockPos.ZERO)) {
-				be.addLoopingPackage(chainConveyorPackage);
-			} else {
-				be.addTravellingPackage(chainConveyorPackage, selectedConnection);
+					if (selectedConnection.equals(BlockPos.ZERO)) {
+						be.addLoopingPackage(chainConveyorPackage);
+					} else {
+						be.addTravellingPackage(chainConveyorPackage, selectedConnection);
+					}
+				}
 			}
 		}
+
 	}
 }
