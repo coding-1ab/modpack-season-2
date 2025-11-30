559cc80f5320272709f3cdff10abf745ce9074f1
cake <cakeistastydev@gmail.com>
2025-11-30 13:41:47 +0000

    Chain "crafting"

--- mods/Create-Bits-n-Bobs/src/main/java/com/kipti/bnb/content/chain_pulley/ChainPulleyBlock.java
+++ mods/Create-Bits-n-Bobs/src/main/java/com/kipti/bnb/content/chain_pulley/ChainPulleyBlock.java
@@ -1,11 +1,17 @@
 package com.kipti.bnb.content.chain_pulley;
 
 import com.kipti.bnb.registry.BnbBlockEntities;
+import com.simibubi.create.AllBlocks;
 import com.simibubi.create.content.contraptions.pulley.PulleyBlock;
 import com.simibubi.create.content.contraptions.pulley.PulleyBlockEntity;
+import com.simibubi.create.content.equipment.wrench.IWrenchable;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
 
-public class ChainPulleyBlock extends PulleyBlock {
+public class ChainPulleyBlock extends PulleyBlock implements IWrenchable {
     public ChainPulleyBlock(final Properties properties) {
         super(properties);
     }
@@ -13,5 +19,17 @@ public class ChainPulleyBlock extends PulleyBlock {
     @Override
     public BlockEntityType<? extends PulleyBlockEntity> getBlockEntityType() {
         return BnbBlockEntities.CHAIN_ROPE_PULLEY.get();
+    }
+
+    @Override
+    public InteractionResult onSneakWrenched(final BlockState state, final UseOnContext context) {
+        if (context.getLevel().isClientSide)
+            return InteractionResult.SUCCESS;
+        context.getLevel()
+                .levelEvent(2001, context.getClickedPos(), Block.getId(state));
+        context.getLevel()
+                .setBlockAndUpdate(context.getClickedPos(), AllBlocks.ROPE_PULLEY.get().defaultBlockState()
+                        .setValue(PulleyBlock.HORIZONTAL_AXIS, state.getValue(PulleyBlock.HORIZONTAL_AXIS)));
+        return InteractionResult.SUCCESS;
     }
 }
