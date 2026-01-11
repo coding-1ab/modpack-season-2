48ac32a23b2c5ba53097a2b2a87cfeb1b98c4f7d
IThundxr <me@ithundxr.dev>
2026-01-11 11:33:43 -0500

    Setting some limits
    - Limit the maxAssemblyLength to 512 and maxBogeyCount to 200 to prevent issues such as #9626

--- mods/Create/src/main/java/com/simibubi/create/content/trains/station/StationBlockEntity.java
+++ mods/Create/src/main/java/com/simibubi/create/content/trains/station/StationBlockEntity.java
@@ -561,11 +561,10 @@ public class StationBlockEntity extends SmartBlockEntity implements Transformabl
 
 		BlockPos bogeyOffset = BlockPos.containing(track.getUpNormal(level, targetPosition, trackState));
 
-		int MAX_LENGTH = AllConfigs.server().trains.maxAssemblyLength.get();
-		int MAX_BOGEY_COUNT = AllConfigs.server().trains.maxBogeyCount.get();
+		int maxLength = AllConfigs.server().trains.maxAssemblyLength.get();
+		int maxBogeyCount = AllConfigs.server().trains.maxBogeyCount.get();
 
 		int bogeyIndex = 0;
-		int maxBogeyCount = MAX_BOGEY_COUNT;
 		if (bogeyLocations == null)
 			bogeyLocations = new int[maxBogeyCount];
 		if (bogeyTypes == null)
@@ -576,8 +575,8 @@ public class StationBlockEntity extends SmartBlockEntity implements Transformabl
 		Arrays.fill(bogeyTypes, null);
 		Arrays.fill(upsideDownBogeys, false);
 
-		for (int i = 0; i < MAX_LENGTH; i++) {
-			if (i == MAX_LENGTH - 1) {
+		for (int i = 0; i < maxLength; i++) {
+			if (i == maxLength - 1) {
 				assemblyLength = i;
 				break;
 			}
