d0e40e5754e125746dd712774d23b9437e314708
banna12345bob <banna12345bob@outlook.com>
2026-02-28 17:09:20 +1000

    Fix ticking occuring on other entities

--- mods/create-new-age/common/src/main/java/org/antarcticgardens/cna/content/nuclear/RadiationPoisoningEffect.java
+++ mods/create-new-age/common/src/main/java/org/antarcticgardens/cna/content/nuclear/RadiationPoisoningEffect.java
@@ -1,13 +1,12 @@
 package org.antarcticgardens.cna.content.nuclear;
 
-import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import org.antarcticgardens.cna.CNASounds;
-import org.antarcticgardens.cna.CreateNewAge;
 import org.antarcticgardens.cna.config.CNAConfig;
 
 public class RadiationPoisoningEffect extends MobEffect {
@@ -20,7 +19,7 @@ public class RadiationPoisoningEffect extends MobEffect {
         if (!livingEntity.level().isClientSide) {
             if (CNAConfig.getCommon().geigerCounterSounds.get()) {
                 float clickChance = 0.15f + (amplifier * 0.10f);
-                if (livingEntity.getRandom().nextFloat() < clickChance) {
+                if (livingEntity.getRandom().nextFloat() < clickChance && livingEntity.getType() == EntityType.PLAYER) {
                     float pitch = 0.95f + livingEntity.getRandom().nextFloat() * 0.1f;
                     CNASounds.GEIGER_COUNTER.playOnServer(livingEntity.level(), livingEntity.blockPosition(), .75f, pitch);
                 }
