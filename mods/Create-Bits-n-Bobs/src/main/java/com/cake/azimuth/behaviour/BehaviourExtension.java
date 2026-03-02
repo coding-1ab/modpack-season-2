2a4dc169469fd5d3d24e61c0e290e6fe7b94e680
cake <cakeistastydev@gmail.com>
2026-03-02 16:55:46 +0000

    Move azimuth to dedicated lib

--- mods/Create-Bits-n-Bobs/src/main/java/com/cake/azimuth/behaviour/BehaviourExtension.java
+++
@@ -1,9 +0,0 @@
-package com.cake.azimuth.behaviour;
-
-/**
- * Common interface for extensions.
- * Extensions are used to selectively add functions to {@link SuperBlockEntityBehaviour}s,
- * specifically to systems where performance is important, and behaviours not requiring this extension are safely ignored.
- */
-public interface BehaviourExtension {
-}
