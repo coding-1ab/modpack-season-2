91ed984df168c080cfe15ac81483323228c4f6f9
cake <cakeistastydev@gmail.com>
2026-03-05 20:30:48 +0000

    Remove unnecassary logging

--- mods/Create-Bits-n-Bobs/src/main/java/com/kipti/bnb/content/kinetics/cogwheel_chain/migration/ChainMigrationEvents.java
+++ mods/Create-Bits-n-Bobs/src/main/java/com/kipti/bnb/content/kinetics/cogwheel_chain/migration/ChainMigrationEvents.java
@@ -15,17 +15,13 @@ public class ChainMigrationEvents {
     public static void onRegisterAliasEvent(final RegisterEvent event) {
         if (event.getRegistry().key() == Registries.BLOCK) {
             for (final Map.Entry<ResourceLocation, ResourceLocation> entry : ChainIdMigrations.BLOCK_RENAMES.entrySet())
-                addAliasLogged(event, entry.getKey(), entry.getValue());
+                event.getRegistry().addAlias(entry.getKey(), entry.getValue());
+
         }
         if (event.getRegistry().key() == Registries.BLOCK_ENTITY_TYPE) {
             for (final Map.Entry<ResourceLocation, ResourceLocation> entry : ChainIdMigrations.BLOCK_ENTITY_RENAMES.entrySet())
-                addAliasLogged(event, entry.getKey(), entry.getValue());
+                event.getRegistry().addAlias(entry.getKey(), entry.getValue());
         }
-    }
-
-    private static void addAliasLogged(final RegisterEvent event, final ResourceLocation a, final ResourceLocation b) {
-        event.getRegistry().addAlias(a, b);
-        System.out.println("!!!! Registered alias: " + a + " → " + b);
     }
 
 }
