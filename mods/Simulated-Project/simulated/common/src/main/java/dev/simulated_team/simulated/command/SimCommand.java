0e78015d88aee6c949f0df9c85b6875d7bd2cb07
KyanBirb <97422254+KyanBirb@users.noreply.github.com>
2026-04-19 17:35:32 -0500

    Add /simulated glue command

--- mods/Simulated-Project/simulated/common/src/main/java/dev/simulated_team/simulated/command/SimCommand.java
+++ mods/Simulated-Project/simulated/common/src/main/java/dev/simulated_team/simulated/command/SimCommand.java
@@ -6,14 +6,18 @@ import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.simibubi.create.content.contraptions.glue.SuperGlueEntity;
 import dev.ryanhcode.sable.api.command.SubLevelArgumentType;
 import dev.ryanhcode.sable.sublevel.ServerSubLevel;
+import dev.simulated_team.simulated.content.entities.honey_glue.HoneyGlueEntity;
 import dev.simulated_team.simulated.content.physics_staff.PhysicsStaffServerHandler;
-import dev.simulated_team.simulated.data.SimLang;
 import net.minecraft.commands.CommandBuildContext;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
+import net.minecraft.commands.arguments.coordinates.BlockPosArgument;
+import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerLevel;
 
 import java.util.Collection;
 
@@ -32,9 +36,28 @@ public class SimCommand {
                         .then(Commands.argument("sub_levels", SubLevelArgumentType.subLevels())
                                 .executes(ctx -> lockSubLevels(ctx, true))
                                 .then(Commands.argument("locked", BoolArgumentType.bool())
-                                        .executes(ctx -> lockSubLevels(ctx, false)))));
+                                        .executes(ctx -> lockSubLevels(ctx, false)))))
+                .then(Commands.literal("glue")
+                        .requires(command -> command.hasPermission(2))
+                        .then(Commands.argument("from", BlockPosArgument.blockPos())
+                                .then(Commands.argument("to", BlockPosArgument.blockPos())
+                                        .executes(SimCommand::glueArea)))
+                )
+                ;
 
         dispatcher.register(cmd);
+    }
+
+    private static int glueArea(CommandContext<CommandSourceStack> ctx) throws CommandSyntaxException {
+        BlockPos from = BlockPosArgument.getLoadedBlockPos(ctx, "from");
+        BlockPos to = BlockPosArgument.getLoadedBlockPos(ctx, "to");
+
+        ServerLevel world = ctx.getSource()
+                .getLevel();
+
+        HoneyGlueEntity entity = new HoneyGlueEntity(world, SuperGlueEntity.span(from, to));
+        world.addFreshEntity(entity);
+        return 1;
     }
 
     private static int lockSubLevels(CommandContext<CommandSourceStack> ctx, boolean toggle) throws CommandSyntaxException {
