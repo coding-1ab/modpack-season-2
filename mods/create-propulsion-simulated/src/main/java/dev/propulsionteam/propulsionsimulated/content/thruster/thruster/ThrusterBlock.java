04f0ade5e66aa55dfcbde671732aa3dc0a91359a
Snowly <visionhyronic@gmail.com>
2026-04-29 12:13:01 +0200

    fix: Thruster collissions

--- mods/create-propulsion-simulated/src/main/java/dev/propulsionteam/propulsionsimulated/content/thruster/thruster/ThrusterBlock.java
+++ mods/create-propulsion-simulated/src/main/java/dev/propulsionteam/propulsionsimulated/content/thruster/thruster/ThrusterBlock.java
@@ -3,15 +3,20 @@ package dev.propulsionteam.propulsionsimulated.content.thruster.thruster;
 import dev.propulsionteam.propulsionsimulated.registries.PropulsionBlockEntities;
 import dev.propulsionteam.propulsionsimulated.content.thruster.AbstractThrusterBlock;
 import dev.propulsionteam.propulsionsimulated.content.thruster.AbstractThrusterBlockEntity;
+import dev.propulsionteam.propulsionsimulated.content.thruster.ThrusterShapes;
 import com.simibubi.create.foundation.blockEntity.SmartBlockEntityTicker;
 import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.DirectionalBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -21,6 +26,15 @@ public class ThrusterBlock extends AbstractThrusterBlock {
 
     public ThrusterBlock(Properties properties) {
         super(properties);
+    }
+
+    @Override
+    protected VoxelShape getShape(final BlockState state,
+                                  final BlockGetter level,
+                                  final BlockPos pos,
+                                  final CollisionContext context) {
+        final Direction direction = state.getValue(FACING);
+        return ThrusterShapes.THRUSTER.get(direction);
     }
 
     @Override
