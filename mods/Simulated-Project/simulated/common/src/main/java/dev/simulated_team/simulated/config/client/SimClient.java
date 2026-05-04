06d62c4c63ea836d5fa576eb78693c5bbd363783
cyvack <cyvack@outlook.com>
2026-05-04 06:37:56 -0700

    Shut up IJ

--- mods/Simulated-Project/simulated/common/src/main/java/dev/simulated_team/simulated/config/client/SimClient.java
+++ mods/Simulated-Project/simulated/common/src/main/java/dev/simulated_team/simulated/config/client/SimClient.java
@@ -3,12 +3,14 @@ package dev.simulated_team.simulated.config.client;
 import dev.simulated_team.simulated.config.client.items.SimItemConfigs;
 import dev.simulated_team.simulated.config.client.block.SimBlockConfigs;
 import net.createmod.catnip.config.ConfigBase;
+import org.jspecify.annotations.NonNull;
 
 public class SimClient extends ConfigBase {
     public final SimItemConfigs itemConfig = this.nested(0, SimItemConfigs::new, SimClient.Comments.itemConfig);
     public final SimBlockConfigs miscConfig = this.nested(0, SimBlockConfigs::new, SimClient.Comments.blockConfig);
 
     @Override
+    @NonNull
     public String getName() {
         return "client";
     }
--- mods/Simulated-Project/simulated/common/src/main/java/dev/simulated_team/simulated/config/client/block/SimBlockConfigs.java
+++ mods/Simulated-Project/simulated/common/src/main/java/dev/simulated_team/simulated/config/client/block/SimBlockConfigs.java
@@ -1,12 +1,14 @@
 package dev.simulated_team.simulated.config.client.block;
 
 import net.createmod.catnip.config.ConfigBase;
+import org.jspecify.annotations.NonNull;
 
 public class SimBlockConfigs extends ConfigBase {
     public final ConfigInt steeringWheelXOffset = this.i(0, -1000, 1000, "steering_wheel_x_offset", SimBlockConfigs.Comments.steeringWheelXOffset);
     public final ConfigInt steeringWheelYOffset = this.i(0, -1000, 1000, "steering_wheel_y_offset", SimBlockConfigs.Comments.steeringWheelYOffset);
 
     @Override
+    @NonNull
     public String getName() {
         return "block";
     }
