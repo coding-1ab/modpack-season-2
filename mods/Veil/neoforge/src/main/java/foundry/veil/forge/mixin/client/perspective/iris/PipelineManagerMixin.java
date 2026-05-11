fbff00032f9749e841da22cfea314483eca7e61d
tmvkrpxl0 <tmvkrpxl0@proton.me>
2026-05-12 00:17:13 +0900

    Fix merge issue - re-fix iris mixin issue later

--- mods/Veil/neoforge/src/main/java/foundry/veil/forge/mixin/client/perspective/iris/PipelineManagerMixin.java
+++ mods/Veil/neoforge/src/main/java/foundry/veil/forge/mixin/client/perspective/iris/PipelineManagerMixin.java
@@ -1,6 +1,5 @@
 package foundry.veil.forge.mixin.client.perspective.iris;
 
-import foundry.veil.VeilMixinPlugin;
 import foundry.veil.api.client.render.VeilLevelPerspectiveRenderer;
 import net.irisshaders.iris.pipeline.PipelineManager;
 import net.irisshaders.iris.shaderpack.materialmap.NamespacedId;
@@ -8,7 +7,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
-@VeilMixinPlugin.ModMixin(modId = "iris")
 @Mixin(PipelineManager.class)
 public class PipelineManagerMixin {
 
