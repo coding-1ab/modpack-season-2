1c9c9c2a9c7e178a0323973a7fe2b4d0ba1fc973
KyivSec <st7321445@stud.duikt.edu.ua>
2026-05-14 20:13:06 +0300

    liquid vector thruster fix

--- mods/create-propulsion-simulated/src/main/java/dev/propulsionteam/propulsionsimulated/content/thruster/liquid_vector_thruster/LiquidVectorThrusterBlock.java
+++ mods/create-propulsion-simulated/src/main/java/dev/propulsionteam/propulsionsimulated/content/thruster/liquid_vector_thruster/LiquidVectorThrusterBlock.java
@@ -6,8 +6,14 @@ import com.mojang.serialization.MapCodec;
 import dev.propulsionteam.propulsionsimulated.content.thruster.AbstractThrusterBlockEntity;
 import dev.propulsionteam.propulsionsimulated.content.thruster.vector_thruster.VectorThrusterBlock;
 import dev.propulsionteam.propulsionsimulated.registries.PropulsionBlockEntities;
+import com.simibubi.create.foundation.blockEntity.SmartBlockEntityTicker;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.DirectionalBlock;
 import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+import javax.annotation.Nonnull;
 
 public class LiquidVectorThrusterBlock extends VectorThrusterBlock {
     public static final MapCodec<LiquidVectorThrusterBlock> CODEC = simpleCodec(LiquidVectorThrusterBlock::new);
@@ -31,5 +37,13 @@ public class LiquidVectorThrusterBlock extends VectorThrusterBlock {
     @Override
     public BlockEntityType<? extends LiquidVectorThrusterBlockEntity> getBlockEntityType() {
         return PropulsionBlockEntities.LIQUID_VECTOR_THRUSTER_BLOCK_ENTITY.get();
+    }
+
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(@Nonnull Level level, @Nonnull BlockState state, @Nonnull BlockEntityType<T> type) {
+        if (type == PropulsionBlockEntities.LIQUID_VECTOR_THRUSTER_BLOCK_ENTITY.get()) {
+            return new SmartBlockEntityTicker<>();
+        }
+        return null;
     }
 }
