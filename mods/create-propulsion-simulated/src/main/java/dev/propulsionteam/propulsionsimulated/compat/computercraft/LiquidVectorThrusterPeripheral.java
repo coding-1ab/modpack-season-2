f7a8e5566ba07c351daae39cc1602ccd2662af03
KyivSec <st7321445@stud.duikt.edu.ua>
2026-05-15 18:41:32 +0300

    CC fluid API passthrough to the liquid vector peripheral

--- mods/create-propulsion-simulated/src/main/java/dev/propulsionteam/propulsionsimulated/compat/computercraft/LiquidVectorThrusterPeripheral.java
+++ mods/create-propulsion-simulated/src/main/java/dev/propulsionteam/propulsionsimulated/compat/computercraft/LiquidVectorThrusterPeripheral.java
@@ -1,14 +1,22 @@
 package dev.propulsionteam.propulsionsimulated.compat.computercraft;
 
 import com.simibubi.create.compat.computercraft.implementation.peripherals.SyncedPeripheral;
+import dan200.computercraft.api.lua.LuaException;
 import dan200.computercraft.api.lua.LuaFunction;
 import dan200.computercraft.api.peripheral.IComputerAccess;
+import dan200.computercraft.shared.peripheral.generic.methods.FluidMethods;
 import dev.propulsionteam.propulsionsimulated.content.thruster.AbstractThrusterBlockEntity.ControlMode;
 import dev.propulsionteam.propulsionsimulated.content.thruster.liquid_vector_thruster.LiquidVectorThrusterBlockEntity;
 import net.minecraft.util.Mth;
+import net.neoforged.neoforge.fluids.capability.IFluidHandler;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Map;
+import java.util.Optional;
+
 public class LiquidVectorThrusterPeripheral extends SyncedPeripheral<LiquidVectorThrusterBlockEntity> {
+    private final FluidMethods fluidMethods = new FluidMethods();
+
     public LiquidVectorThrusterPeripheral(LiquidVectorThrusterBlockEntity blockEntity) {
         super(blockEntity);
     }
@@ -81,6 +89,32 @@ public class LiquidVectorThrusterPeripheral extends SyncedPeripheral<LiquidVecto
     @LuaFunction
     public final double getPower() {
         return blockEntity.getPower();
+    }
+
+    // IFluidHandler methods passthrough
+    @LuaFunction(mainThread = true)
+    public final Map<Integer, Map<String, ?>> tanks() throws LuaException {
+        IFluidHandler handler = getHandler();
+        return this.fluidMethods.tanks(handler);
+    }
+
+    @LuaFunction(mainThread = true)
+    public final int pushFluid(IComputerAccess computer, String toName, Optional<Integer> limit, Optional<String> fluidName) throws LuaException {
+        IFluidHandler handler = getHandler();
+        return this.fluidMethods.pushFluid(handler, computer, toName, limit, fluidName);
+    }
+
+    @LuaFunction(mainThread = true)
+    public final int pullFluid(IComputerAccess computer, String fromName, Optional<Integer> limit, Optional<String> fluidName) throws LuaException {
+        IFluidHandler handler = getHandler();
+        return this.fluidMethods.pullFluid(handler, computer, fromName, limit, fluidName);
+    }
+
+    private IFluidHandler getHandler() throws LuaException {
+        IFluidHandler handler = blockEntity.getFluidHandler(blockEntity.getFacing());
+        if (handler == null)
+            throw new LuaException("Fluid tank not available");
+        return handler;
     }
 
     @Override
